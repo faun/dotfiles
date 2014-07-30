5f1dad78ed8acbcfef1513f8627eba5b31aec3cd
Faun <faun@riseup.net>
2014-07-30 22:01:30 +0700

    Update ruby version manager loading code
    - Move to shrc
    - Add customizable rvm or rbenv paths

--- shrc/config.sh
+++ shrc/config.sh
@@ -28,6 +28,33 @@ elif [[ $platform == 'Darwin' ]]; then
   fi
 fi
 
+
+# enable rvm if it exists
+if [[ "x$RVM_ROOT" == "x" ]]
+then
+  export RVM_ROOT="$HOME/.rvm"
+fi
+
+if [ -d $RVM_ROOT ]; then
+  export PATH=$RVM_ROOT/bin:$PATH
+  export rvm_path="$RVM_ROOT"
+  [[ -s $rvm_path/scripts/rvm ]] && source $rvm_path/scripts/rvm
+fi
+
+# otherwise use rbenv
+if [[ "x$RBENV_ROOT" == "x" ]]
+then
+  export RBENV_ROOT=$HOME/.rbenv
+fi
+
+if [ -d $RBENV_ROOT ]; then
+  export PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"
+fi
+
+if which rbenv > /dev/null; then
+  eval "$(rbenv init -)"
+fi
+
 if [ -f $HOME/.local.sh ]; then
   source $HOME/.local.sh
 fi
--- zshrc
+++ zshrc
@@ -18,23 +18,6 @@ for file in $HOME/.zsh/*.sh; do
   source "$file"
 done
 
-# enable rvm if it exists
-if [ -d $HOME/.rvm ]; then
-  export PATH=$HOME/.rvm/bin:$PATH
-  export rvm_path="$HOME/.rvm"
-  [[ -s $rvm_path/scripts/rvm ]] && source $rvm_path/scripts/rvm
-fi
-
-# otherwise use rbenv
-
-if [ -d $HOME/.rbenv ]; then
-  export PATH="$HOME/.rbenv/bin:$HOME/.rbenv/shims:$PATH"
-fi
-if which rbenv > /dev/null; then
-  eval "$(rbenv init -)"
-  export RBENV_ROOT=$HOME/.rbenv
-fi
-
 source $HOME/.zsh/faunzy.zsh-theme
 
 bindkey "^[b" backward-word
