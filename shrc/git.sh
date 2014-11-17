eac7d4d44ba7424692be70635747aededc1a06d7
Faun <github@faun.me>
2014-11-16 21:50:03 -0800

    Remove aliases that I use too much

--- shrc/git.sh
+++ shrc/git.sh
@@ -3,7 +3,6 @@ alias got='git'
 alias get='git'
 
 # git status
-alias gs='git status'
 alias g='git status -sb'
 
 # git pull
@@ -67,7 +66,6 @@ alias stashpop="git stash && git pull && git stash pop"
 alias grm='git status --porcelain | ruby -e "puts STDIN.read.scan(/^\\s+D\\s+(.+)\$/).join(\"\\n\")" | xargs git rm'
 
 alias gg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %aN: %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias lg="gg"
 alias ggg="git log --graph --pretty=format:'%C(yellow)%h %Creset(%cr)%nAuthor: %C(green)%aN <%aE>%Creset%n%n    %Cblue%s%Creset%n'"
 alias gggg="git log --pretty=format:'%C(yellow)%h %Creset(%cr) %C(green)%aN <%aE>%Creset%n%Cblue%s%Creset%n ' --numstat"
 alias gitmine="git log --author='$(git config --get user.name)' --pretty=format:'%Cgreen%ad%Creset %s%C(yellow)%d%Creset %Cred(%h)%Creset' --date=short"
