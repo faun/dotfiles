7e83df94dfeb6c3240e12ac6b3f6e471323ec5d6
Faun <github@faun.me>
2014-12-28 16:02:56 -0800

    Test for boot2docker status before configuring

--- shrc/_.paths.sh
+++ shrc/_.paths.sh
@@ -22,6 +22,9 @@ export NODE_PATH="/usr/local/lib/node_modules"
 export GOPATH="$HOME/go"
 export PATH=$PATH:$GOPATH/bin
 
-export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
-export LANG=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
+if [[ "$(boot2docker status)" -eq "running" ]]
+then
+  export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
+  export LANG=en_US.UTF-8
+  export LC_CTYPE=en_US.UTF-8
+fi
