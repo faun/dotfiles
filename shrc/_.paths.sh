0226b537c0e117d899281e4d13159344209b9734
Faun <github@faun.me>
2015-02-05 10:13:08 -0800

    Ensure boot2docker is available

--- shrc/_.paths.sh
+++ shrc/_.paths.sh
@@ -22,9 +22,12 @@ export NODE_PATH="/usr/local/lib/node_modules"
 export GOPATH="$HOME"
 export PATH=$PATH:$GOPATH/bin
 
-if [[ "$(boot2docker status)" -eq "running" ]]
+if $(which boot2docker > /dev/null 2>&1)
 then
-  export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
-  export LANG=en_US.UTF-8
-  export LC_CTYPE=en_US.UTF-8
+  if [[ "$(boot2docker status)" -eq "running" ]]
+  then
+    export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
+    export LANG=en_US.UTF-8
+    export LC_CTYPE=en_US.UTF-8
+  fi
 fi
