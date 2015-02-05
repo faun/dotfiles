924b48702df2dfc7afe80513fca8a24cc55fb835
Faun <github@faun.me>
2015-02-05 10:28:44 -0800

    Cleanup aliases
    - Clean up useless aliases
    - Remove FreeBSD branch
    - Move MacOS specific aliases to a separate file

---
+++ shrc/optional/macos.sh
@@ -0,0 +1,6 @@
+alias show_invisibles='defaults write com.Apple.Finder AppleShowAllFiles true'
+alias hide_invisibles='defaults write com.Apple.Finder AppleShowAllFiles false'
+alias open_html='open -a Safari build/*/index_us.html'
+alias lock="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
+alias flushdns="sudo discoveryutil mdnsflushcache"
+alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
