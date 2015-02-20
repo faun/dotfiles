9a8ce23e27bd64a403fc0089eb5c00e9bff5961d
Faun <github@faun.me>
2015-02-19 23:04:03 -0800

    Remove guard

--- zsh/config.sh
+++ zsh/config.sh
@@ -1,65 +1,62 @@
-if [[ $(echo "$SHELL" | grep "/zsh$" > /dev/null) ]]
-then
-  if [[ -n $SSH_CONNECTION ]]; then
-    export PS1='%m:%3~$(git_info_for_prompt)%# '
-  else
-    export PS1='%3~$(git_info_for_prompt)%# '
-  fi
-  record_time "zsh prompt"
-
-  fpath=(~/.zsh/functions $fpath)
-  autoload -U ~/.zsh/functions/*(:t)
-
-  HISTFILE=~/.zsh_history
-  HISTSIZE=1000
-  SAVEHIST=1000
-
-  setopt NO_BG_NICE # don't nice background tasks
-  setopt NO_HUP
-  setopt NO_LIST_BEEP
-  setopt LOCAL_OPTIONS # allow functions to have local options
-  setopt LOCAL_TRAPS # allow functions to have local traps
-  setopt HIST_VERIFY
-  setopt SHARE_HISTORY # share history between sessions ???
-  setopt EXTENDED_HISTORY # add timestamps to history
-  setopt PROMPT_SUBST
-  setopt CORRECT
-  setopt COMPLETE_IN_WORD
-  setopt RMSTARSILENT # don't confirm an rm *
-  setopt CDABLE_VARS # set the option so that no '$' is required when using the save function
-
-  setopt APPEND_HISTORY # adds history
-  setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-  setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-  setopt HIST_REDUCE_BLANKS
-
-  unsetopt correct_all # don't autocorrect
-  unsetopt correct
-
-  zle -N newtab
-
-  bindkey '^[^[[D' backward-word
-  bindkey '^[^[[C' forward-word
-  bindkey '^[b' backward-word
-  bindkey '^[w' forward-word
-  bindkey '^[[5D' beginning-of-line
-  bindkey '^[[5C' end-of-line
-  bindkey '^[[3~' delete-char
-  bindkey '^[^N' newtab
-  bindkey '^?' backward-delete-char
-  bindkey '^O' clear-screen
-  record_time "bindings"
-
-  # Set screen titles to last run command
-  setopt extended_glob
-  preexec () {
-    if [[ "$TERM" == "screen" ]]; then
-      local CMD=${1[(wr)^(*=*|sudo|-*)]}
-      echo -ne "\ek$CMD\e\\"
-    fi
-  }
-
-  unalias md
-  alias help="man zshbuiltins"
+if [[ -n $SSH_CONNECTION ]]; then
+  export PS1='%m:%3~$(git_info_for_prompt)%# '
+else
+  export PS1='%3~$(git_info_for_prompt)%# '
 fi
+record_time "zsh prompt"
+
+fpath=(~/.zsh/functions $fpath)
+autoload -U ~/.zsh/functions/*(:t)
+
+HISTFILE=~/.zsh_history
+HISTSIZE=1000
+SAVEHIST=1000
+
+setopt NO_BG_NICE # don't nice background tasks
+setopt NO_HUP
+setopt NO_LIST_BEEP
+setopt LOCAL_OPTIONS # allow functions to have local options
+setopt LOCAL_TRAPS # allow functions to have local traps
+setopt HIST_VERIFY
+setopt SHARE_HISTORY # share history between sessions ???
+setopt EXTENDED_HISTORY # add timestamps to history
+setopt PROMPT_SUBST
+setopt CORRECT
+setopt COMPLETE_IN_WORD
+setopt RMSTARSILENT # don't confirm an rm *
+setopt CDABLE_VARS # set the option so that no '$' is required when using the save function
+
+setopt APPEND_HISTORY # adds history
+setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
+setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
+setopt HIST_REDUCE_BLANKS
+
+unsetopt correct_all # don't autocorrect
+unsetopt correct
+
+zle -N newtab
+
+bindkey '^[^[[D' backward-word
+bindkey '^[^[[C' forward-word
+bindkey '^[b' backward-word
+bindkey '^[w' forward-word
+bindkey '^[[5D' beginning-of-line
+bindkey '^[[5C' end-of-line
+bindkey '^[[3~' delete-char
+bindkey '^[^N' newtab
+bindkey '^?' backward-delete-char
+bindkey '^O' clear-screen
+record_time "bindings"
+
+# Set screen titles to last run command
+setopt extended_glob
+preexec () {
+  if [[ "$TERM" == "screen" ]]; then
+    local CMD=${1[(wr)^(*=*|sudo|-*)]}
+    echo -ne "\ek$CMD\e\\"
+  fi
+}
+
+unalias md
+alias help="man zshbuiltins"
 record_time "zsh config"
