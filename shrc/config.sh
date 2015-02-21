64770ba7fe4ae5b897e2907879cd63a6e949d270
Faun <github@faun.me>
2015-02-21 12:21:03 -0800

    Clarify comment

--- shrc/config.sh
+++ shrc/config.sh
@@ -3,7 +3,7 @@ export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
 export EDITOR='vim'
 
-# Handle the fact that this file will be used with multiple OSs
+# Load optional platform-specific configuration
 platform=`uname`
 if [[ $platform == 'Linux' ]]; then
   alias a='ls -lrth --color'
