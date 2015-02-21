f8363042daeff8399602aeddc4cdb1167fef553f
Faun <github@faun.me>
2015-02-21 13:07:23 -0800

    Prevent loading duplicate paths
    Test for $TMUX before loading paths

--- shrc/_.paths.sh
+++ shrc/_.paths.sh
@@ -1,16 +1,19 @@
-PATH="~/bin:$PATH"
-PATH=".git/safe/../../bin:$PATH"
-PATH="/usr/local/heroku/bin:$PATH"
-PATH="/usr/local/bin:/usr/local/sbin:$PATH"
-PATH="/opt/boxen/bin:/opt/boxen/homebrew/sbin:/opt/boxen/homebrew/bin:$PATH"
-PATH="/usr/local/mysql/bin:$PATH"
-PATH="/usr/local/git/bin:$PATH"
-export PATH
+if [[ -z $TMUX ]]
+then
+  PATH="~/bin:$PATH"
+  PATH=".git/safe/../../bin:$PATH"
+  PATH="/usr/local/heroku/bin:$PATH"
+  PATH="/usr/local/bin:/usr/local/sbin:$PATH"
+  PATH="/opt/boxen/bin:/opt/boxen/homebrew/sbin:/opt/boxen/homebrew/bin:$PATH"
+  PATH="/usr/local/mysql/bin:$PATH"
+  PATH="/usr/local/git/bin:$PATH"
+  export PATH
 
-MANPATH="/usr/local/man:$MANPATH"
-MANPATH="/usr/local/mysql/man:$MANPATH"
-MANPATH="/usr/local/git/man:$MANPATH"
-export MANPATH
+  MANPATH="/usr/local/man:$MANPATH"
+  MANPATH="/usr/local/mysql/man:$MANPATH"
+  MANPATH="/usr/local/git/man:$MANPATH"
+  export MANPATH
 
-export NODE_PATH="/usr/local/lib/node_modules"
-export GOPATH="$HOME"
+  export NODE_PATH="/usr/local/lib/node_modules"
+  export GOPATH="$HOME"
+fi
