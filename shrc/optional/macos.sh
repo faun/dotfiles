1934a772c5a57d6e5efec4b872fc4a97c006db37
Faun <github@Faun.me>
2015-04-21 21:26:41 -0700

    Un-break docker exports

--- shrc/optional/macos.sh
+++ shrc/optional/macos.sh
@@ -5,11 +5,12 @@ alias lock="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Res
 alias flushdns="sudo discoveryutil mdnsflushcache"
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 
-if ps -ax | grep '[b]oot2docker' 2>&1 /dev/null
+if ps -ax | grep '[b]oot2docker' > /dev/null
 then
-  if [[ "$(boot2docker status)" -eq "running" ]]
+  if [[ "$(boot2docker status)" == "running" ]]
   then
-    export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
+    $(boot2docker shellinit 2> /dev/null)
+    export DOCKER_IP=$(boot2docker ip 2>/dev/null)
   fi
 fi
 
