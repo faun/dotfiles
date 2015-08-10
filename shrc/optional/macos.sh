2974e4e3c46c01fce1b7b1b474c7f7e2f0e3a29c
Faun <github@faun.me>
2015-08-10 09:46:26 -0700

    Change flushdns command to match latest OS
    https://support.apple.com/en-us/HT202516

--- shrc/optional/macos.sh
+++ shrc/optional/macos.sh
@@ -2,7 +2,7 @@ alias show_invisibles='defaults write com.Apple.Finder AppleShowAllFiles true'
 alias hide_invisibles='defaults write com.Apple.Finder AppleShowAllFiles false'
 alias open_html='open -a Safari build/*/index_us.html'
 alias lock="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
-alias flushdns="sudo discoveryutil mdnsflushcache"
+alias flushdns="sudo killall -HUP mDNSResponder"
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 
 if ps -ax | grep '[b]oot2docker' > /dev/null
