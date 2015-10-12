893977841a781857a3d055645be0939e54dffc3b
Faun <github@faun.me>
2015-10-11 17:21:53 -0700

    Remove dollar alias

--- shrc/aliases.sh
+++ shrc/aliases.sh
@@ -38,7 +38,6 @@ alias ducks='du -cksh * | sort -rn|head -11' # Lists folders and files sizes in 
 alias m='more'
 alias df='df -h'
 alias lm='!! | more'
-alias '$'=' '
 
 alias py='python'
 alias sha1="openssl sha1"
