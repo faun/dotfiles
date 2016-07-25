64114c2ddb0d155dbfe6f4744854b1e91cb7f66f
Faun <github@faun.me>
2016-07-24 22:26:25 -0700

    Remove docker-machine config

--- shrc/optional/macos.sh
+++ shrc/optional/macos.sh
@@ -5,18 +5,6 @@ alias lock="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Res
 alias flushdns="sudo killall -HUP mDNSResponder"
 alias fixopenwith='/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user'
 
-if ps -ax | grep 'docker-machine' > /dev/null
-then
-  if [[ "$(docker-machine status default)" == "Running" ]]
-  then
-    eval $(docker-machine env default)
-  elif [[ "$(docker-machine status default)" == "Stopped" ]]
-  then
-    echo "Starting Docker..."
-    docker-machine start default && eval $(docker-machine env default)
-  fi
-fi
-
 HOMEBREW_PREFIX="$(brew --prefix)"
 export HOMEBREW_PREFIX
 
