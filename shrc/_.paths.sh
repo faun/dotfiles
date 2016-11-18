225bd0bdb7a7560984bcbf8d11b4052c48a017a5
Faun <github@faun.me>
2016-11-18 09:40:54 -0800

    Reverse path loading order

--- shrc/_.paths.sh
+++ shrc/_.paths.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 if [[ -z $TMUX ]]
 then
-  PATH="$HOME/bin:$PATH"
-  PATH=".git/safe/../../bin:$PATH"
-  PATH="./node_modules/.bin:$PATH"
-  PATH="/usr/local/heroku/bin:$PATH"
-  PATH="/usr/local/bin:/usr/local/sbin:$PATH"
-  PATH="/opt/boxen/bin:/opt/boxen/homebrew/sbin:/opt/boxen/homebrew/bin:$PATH"
-  PATH="/usr/local/mysql/bin:$PATH"
-  PATH="/usr/local/git/bin:$PATH"
+  PATH="$PATH:$HOME/bin"
+  PATH="$PATH:.git/safe/../../bin"
+  PATH="$PATH:.git/safe/../../node_modules/.bin"
+  PATH="$PATH:/usr/local/heroku/bin"
+  PATH="$PATH:/usr/local/bin:/usr/local/sbin"
+  PATH="$PATH:/opt/boxen/bin:/opt/boxen/homebrew/sbin:/opt/boxen/homebrew/bin"
+  PATH="$PATH:/usr/local/mysql/bin"
+  PATH="$PATH:/usr/local/git/bin"
+  PATH="$PATH:$HOME/.cargo/bin"
   PATH="$PATH:$HOME/Library/Python/2.7/bin"
+  PATH="$PATH:$HOME/perl5/bin";
+
   export PATH
 
   MANPATH="/usr/local/man:$MANPATH"
