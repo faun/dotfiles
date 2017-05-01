ef853531886f2454da2cad975710e5e111788a36
Faun <github@faun.me>
2017-04-30 21:20:16 -0700

    Update search colors for man

--- shrc/functions.sh
+++ shrc/functions.sh
@@ -122,14 +122,14 @@ function show_colors() {
 }
 
 man() {
-	env \
-		LESS_TERMCAP_md=$'\e[1;36m' \
-		LESS_TERMCAP_me=$'\e[0m' \
-		LESS_TERMCAP_se=$'\e[0m' \
-		LESS_TERMCAP_so=$'\e[1;40;92m' \
-		LESS_TERMCAP_ue=$'\e[0m' \
-		LESS_TERMCAP_us=$'\e[1;32m' \
-			man "$@"
+  env \
+    LESS_TERMCAP_md=$'\e[1;36m' \
+    LESS_TERMCAP_me=$'\e[0m' \
+    LESS_TERMCAP_so=$'\e[7;33;33m' \
+    LESS_TERMCAP_se=$'\e[0m' \
+    LESS_TERMCAP_ue=$'\e[0m' \
+    LESS_TERMCAP_us=$'\e[1;32m' \
+      man "$@"
 }
 
 heroku_apps() {
