674fa6eb2748c52c8b65b0e68ad68cd9ab7afeff
Faun <github@faun.me>
2017-05-19 23:22:11 -0700

    Add fasd configuration

--- shrc/03_fasd.sh
+++ shrc/03_fasd.sh
@@ -1,3 +1,11 @@
-eval "$(fasd --init auto)"
+fasd_cache="$HOME/.fasd-init-zsh"
+if [ "$(command -v fasd)" -nt "$fasd_cache" ] || [ ! -s "$fasd_cache" ]; then
+  fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install >| "$fasd_cache"
+fi
+
+alias v='f -t -e vim -b viminfo'
+
+source "$fasd_cache"
+unset fasd_cache
 
 record_time "fasd"
