ce840e2eb415ca7b127e41326182e0735863ca4e
Faun <github@faun.me>
2017-06-22 21:50:53 -0700

    Add history substring search

--- zsh/antigen_setup.zsh
+++ zsh/antigen_setup.zsh
@@ -1,5 +1,29 @@
+autoload -Uz antigeninit
+record_time "antigen autoload"
 source $HOME/.config/antigen/antigen.zsh
-antigen bundle zsh-users/zsh-syntax-highlighting
-record_time "zsh-syntax-highlight"
+record_time "antigen config"
+
+antigen_packages=(
+zsh-users/zsh-syntax-highlighting
+zsh-users/zsh-completions
+zsh-users/zsh-syntax-highlighting
+zsh-users/zsh-history-substring-search
+)
+
+for package in $antigen_packages
+do
+  antigen bundle "$package"
+  record_time "$package"
+done
+
 antigen apply
 record_time "antigen apply"
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
+bindkey -M emacs '^P' history-substring-search-up
+bindkey -M emacs '^N' history-substring-search-down
+
+bindkey -M vicmd 'k' history-substring-search-up
+bindkey -M vicmd 'j' history-substring-search-down
