2cb35400a46b343f6122fc53b7c03be8c15b96d4
Faun <github@faun.me>
2017-07-30 14:23:07 -0700

    Update antigen configuration

--- zsh/antigen_setup.zsh
+++ zsh/antigen_setup.zsh
@@ -1,29 +1,34 @@
-autoload -Uz antigeninit
-record_time "antigen autoload"
-source $HOME/.config/antigen/antigen.zsh
-record_time "antigen config"
+if [[ -s $HOME/.config/antigen/antigen.zsh ]]
+then
+  autoload -Uz antigeninit
+  record_time "antigen autoload"
 
-antigen_packages=(
-zsh-users/zsh-syntax-highlighting
-zsh-users/zsh-completions
-zsh-users/zsh-syntax-highlighting
-zsh-users/zsh-history-substring-search
-)
+  source $HOME/.config/antigen/antigen.zsh
+  record_time "antigen config"
 
-for package in $antigen_packages
-do
-  antigen bundle "$package"
-  record_time "$package"
-done
+  antigen_packages=(
+    zsh-users/zsh-syntax-highlighting
+    zsh-users/zsh-completions
+    zsh-users/zsh-syntax-highlighting
+    zsh-users/zsh-history-substring-search
+  )
 
-antigen apply
-record_time "antigen apply"
 
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
+  for package in $antigen_packages
+  do
+    antigen bundle "$package"
+    record_time "$package"
+  done
 
-bindkey -M emacs '^P' history-substring-search-up
-bindkey -M emacs '^N' history-substring-search-down
+  antigen apply
+  record_time "antigen apply"
 
-bindkey -M vicmd 'k' history-substring-search-up
-bindkey -M vicmd 'j' history-substring-search-down
+  bindkey '^[[A' history-substring-search-up
+  bindkey '^[[B' history-substring-search-down
+
+  bindkey -M emacs '^P' history-substring-search-up
+  bindkey -M emacs '^N' history-substring-search-down
+
+  bindkey -M vicmd 'k' history-substring-search-up
+  bindkey -M vicmd 'j' history-substring-search-down
+fi
