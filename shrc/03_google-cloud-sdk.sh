849a0dda4e6b975e91bcc19aaceaa09b0bcb8c6a
Faun <faun@percy.io>
2017-08-28 15:48:45 -0700

    Add gcloud completion setup

---
+++ shrc/03_google-cloud-sdk.sh
@@ -0,0 +1,12 @@
+HOMEBREW_PATH=$(brew --prefix)
+if [[ -d "$HOMEBREW_PATH/Caskroom/google-cloud-sdk/latest/" ]]
+then
+  if [[ -n $ZSH_NAME ]]
+  then
+    source "$HOMEBREW_PATH/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+    source "$HOMEBREW_PATH/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+  else
+    source "$HOMEBREW_PATH/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
+    source "$HOMEBREW_PATH/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
+  fi
+fi
