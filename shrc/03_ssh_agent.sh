1bb56447acf214f387bbdd82d26bc86c6047f0f3
Faun <git@faun.me>
2017-08-29 13:55:16 -0700

    Add keys to ssh-agent automatically

---
+++ shrc/03_ssh_agent.sh
@@ -0,0 +1,21 @@
+SSH_ENV="$HOME/.ssh/environment"
+
+function start_agent {
+  /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
+  chmod 600 "${SSH_ENV}"
+  source "${SSH_ENV}" > /dev/null
+  ssh-add -K ~/.ssh/id_rsa &> /dev/null
+  ssh-add -A &> /dev/null
+}
+
+# Source SSH settings, if applicable
+
+if [ -f "${SSH_ENV}" ]; then
+  source "${SSH_ENV}" > /dev/null
+  #ps ${SSH_AGENT_PID} doesn't work under cywgin
+  ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+  start_agent;
+}
+else
+  start_agent;
+fi
