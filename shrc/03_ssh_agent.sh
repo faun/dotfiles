19c266f919d0ea3a61944b45018c94a825ef5c61
Faun <git@faun.me>
2017-10-14 18:21:58 -0700

    Add comment

--- shrc/03_ssh_agent.sh
+++ shrc/03_ssh_agent.sh
@@ -4,6 +4,7 @@ function start_agent {
   /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
   chmod 600 "${SSH_ENV}"
   source "${SSH_ENV}" > /dev/null
+  # Note this only works on macOS Sierra +
   ssh-add -K ~/.ssh/id_rsa &> /dev/null
   ssh-add -A &> /dev/null
 }
