d949ba28eb9d51c6730ce623433ffa26db5e13fa
Faun <git@faun.me>
2017-11-22 15:53:54 -0800

    Remove fuzzy history search

--- zsh/config.sh
+++ zsh/config.sh
@@ -121,13 +121,6 @@ then
     bindkey "${terminfo[knp]}" down-line-or-history     # [PageDown] - Down a line of history
   fi
 
-  if [[ "${terminfo[kcuu1]}" != "" ]]; then
-    bindkey "${terminfo[kcuu1]}" up-line-or-search      # start typing + [Up-Arrow] - fuzzy find history forward
-  fi
-  if [[ "${terminfo[kcud1]}" != "" ]]; then
-    bindkey "${terminfo[kcud1]}" down-line-or-search    # start typing + [Down-Arrow] - fuzzy find history backward
-  fi
-
   if [[ "${terminfo[khome]}" != "" ]]; then
     bindkey "${terminfo[khome]}" beginning-of-line      # [Home] - Go to beginning of line
   fi
