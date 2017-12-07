f439d3d38341494b7ab0d447a4e406bc816e7ca0
Faun <git@faun.me>
2017-12-07 10:33:39 -0800

    Remove yarn global bin

--- shrc/_.paths.sh
+++ shrc/_.paths.sh
@@ -10,7 +10,6 @@ then
   PATH="$PATH:$HOME/.cargo/bin"
   PATH="$PATH:$HOME/Library/Python/2.7/bin"
   PATH="$PATH:$HOME/perl5/bin";
-  PATH="$PATH:$(yarn global bin)"
   PATH="$HOME/.yarn/bin:$PATH"
   # PATH=".git/safe/../../bin:$PATH"
   # PATH=".git/safe/../../node_modules/.bin:$PATH"
