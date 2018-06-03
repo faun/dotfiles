e9f597ac620bf23601f8f820c871bf020504842e
Faun <git@faun.me>
2018-06-03 16:27:55 -0700

    Install linuxbrew if necessary

--- install.sh
+++ install.sh
@@ -13,6 +13,15 @@ then
   sudo xcodebuild -license accept
 fi
 
+if [[ "$OSTYPE" == linux* ]]; then
+  sudo apt-get install build-essential curl file git
+  git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
+  test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+  test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+  test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+  echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+fi
+
 xcode-select --install > /dev/null 2>&1 || true
 
 # -----------------------------------------------------------------------------
