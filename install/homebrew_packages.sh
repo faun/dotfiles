468ee6c99e46ec1b392e80b1b3e66fef2a9f0e88
Faun <git@faun.me>
2020-10-09 10:02:50 -0700

    Show package install errors

--- install/homebrew_packages.sh
+++ install/homebrew_packages.sh
@@ -10,10 +10,13 @@ homebrew_dependencies=(
 for brew_package in "${homebrew_dependencies[@]}"; do
   if ! brew ls --versions | awk '{ print $1 }' | grep "^$brew_package\$" >/dev/null; then
     echo "Installing package: $brew_package"
-    brew install "$brew_package" 2>/dev/null || true
+    brew install "$brew_package" 2>/tmp/package_error || true
     status=$?
     if [[ status != 0 ]]; then
       echo "Package $brew_package failed to install!"
+      echo ---
+      cat /tmp/package_error
+      echo ---
     fi
   else
     echo "Package $brew_package already installed"
