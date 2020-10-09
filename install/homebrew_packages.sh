2692a08eac440c27c889716e8bfc93dd56de92a5
Faun <git@faun.me>
2020-10-09 10:37:41 -0700

    Capture return values

--- install/homebrew_packages.sh
+++ install/homebrew_packages.sh
@@ -13,8 +13,10 @@ homebrew_dependencies=(
 for brew_package in "${homebrew_dependencies[@]}"; do
   if ! brew ls --versions | awk '{ print $1 }' | grep "^$brew_package\$" >/dev/null; then
     echo "Installing package: $brew_package"
-    brew install "$brew_package" 2>/tmp/package_error || true
+    set +e
+    brew install "$brew_package" 2>/tmp/package_error
     status=$?
+    set -e
     if [[ status != 0 ]]; then
       echo "Package $brew_package failed to install!"
       echo ---
