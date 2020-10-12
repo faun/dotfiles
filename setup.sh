321b7401832232416922394c2b72da7c592a1b4f
Faun <git@faun.me>
2020-10-12 12:26:53 -0700

    Simplify and update setup script

--- setup.sh
+++ setup.sh
@@ -2,26 +2,6 @@ set -e
 
 DESTINATION="$HOME/src/github.com/${USER}/dotfiles"
 RBENV_INSTALL_DIR="$HOME/.rbenv"
-HOMEBREW_INSTALL_SCRIPT='http://git.io/pVOl'
-
-# Install Homebrew
-command -v brew >/dev/null 2>&1 ||
-  ruby -e "$(curl -fsSL $HOMEBREW_INSTALL_SCRIPT)"
-
-# Make the dotfiles folder
-mkdir -p $DESTINATION
-
-# Clone this repository to your machine & initialize submodules
-if ! [[ -d $DESTINATION ]]
-then
-  git clone --recursive git://github.com/faun/dotfiles $DESTINATION
-fi
-
-# Change working directory
-cd $DESTINATION
-
-# Make symlinks to $HOME
-rake
 
 # Install vim bundles
 vim +PlugInstall +qall
@@ -29,11 +9,8 @@ vim +PlugInstall +qall
 # Install prerequisites
 brew install zsh vim ag tmux reattach-to-user-namespace
 
-# Install homebrew-cask
-brew install caskroom/cask/brew-cask
-
 # Install iterm2
-brew cask install iterm2
+brew install iterm2
 
 if ! [[ $SHELL =~ /(.*)/zsh ]]
 then
