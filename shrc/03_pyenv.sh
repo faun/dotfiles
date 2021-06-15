73229ec91b4995b2d5247d591e285d26466b335f
Faun <git@faun.me>
2021-06-15 16:44:14 -0700

    Format with shfmt

--- shrc/03_pyenv.sh
+++ shrc/03_pyenv.sh
@@ -1,16 +1,13 @@
 # Set PYENV_ROOT to ~/.pyenv unless it exists
-if [[ "x$PYENV_ROOT" == "x" ]]
-then
+if [[ "x$PYENV_ROOT" == "x" ]]; then
   export PYENV_ROOT=$HOME/.pyenv
 fi
 record_time "pyenv paths"
 
 if [ -d "$PYENV_ROOT" ]; then
-  if command -v pyenv > /dev/null 2>&1
-  then
+  if command -v pyenv >/dev/null 2>&1; then
     eval "$(pyenv init -)"
     eval "$(pyenv virtualenv-init -)"
   fi
 fi
 record_time "pyenv initialization"
-
