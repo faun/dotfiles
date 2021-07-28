8e5d1834fa2eb81d42a1f6a6fd1d4f8eced09445
Faun <git@faun.me>
2021-07-28 11:16:54 -0700

    Add cargo installer

---
+++ install/install_cargo.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
+
+source "$HOME/.cargo/env"
+
+mkdir -p "$HOME/.zfunc"
+rustup completions zsh > "$HOME/.zfunc/_rustup"
