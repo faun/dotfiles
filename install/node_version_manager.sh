77e072ea45978b503b2dfae0d752f0a2a23ac345
Faun <git@faun.me>
2021-07-28 09:54:23 -0700

    Source n setup after installation

--- install/node_version_manager.sh
+++ install/node_version_manager.sh
@@ -2,16 +2,17 @@
 set -eou pipefail
 
 cd "$(dirname "$0")" || exit 1
-# DIR="$(pwd)"
+DIR="$(pwd)"
 
 # Install n from GitHub
 
 N_PREFIX="${N_PREFIX:-$HOME/n}"
 echo "N_PREFIX: $N_PREFIX"
-if ! [[ -d "$N_PREFIX/n" ]]; then
+if ! [[ -d "$N_PREFIX" ]]; then
   curl -sL https://git.io/n-install | bash -s -- -q
 else
   export N_PREFIX
   "$N_PREFIX/bin/n-update" -y
 fi
 
+source "$DIR/../shrc/03_n.sh"
