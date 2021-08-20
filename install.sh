9700c01b0f80219d9435c3ee1595c057732dea0d
Faun <git@faun.me>
2021-08-20 23:26:25 +0000

    Install n at the correct path

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ touch "$HOME/.vim/spell/en.utf-8.add"
 N_PREFIX="${N_PREFIX:-$HOME/n}"
 echo "N_PREFIX: $N_PREFIX"
 if ! [[ -d "$N_PREFIX/n" ]]; then
-  curl -sL https://git.io/n-install | bash -s -- -q -n
+  curl -sL https://git.io/n-install | N_PREFIX=$N_PREFIX bash -s -- -q -y
 else
   export N_PREFIX
   "$N_PREFIX/bin/n-update" -y
