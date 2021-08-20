3658579c21fdbc8ed1b1e159f246c1e597b04351
Faun <git@faun.me>
2021-08-20 23:30:57 +0000

    Simplify homebrew installation

--- install/homebrew.sh
+++ install/homebrew.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+install_homebrew_if_needed() {
+  if ! command -v brew >/dev/null 2>&1; then
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
+}
+
 if [[ "$OSTYPE" == darwin* ]]; then
   # Accept the XCode license agreement
   XCODE_EXIT_CODE=$(
@@ -14,22 +20,11 @@ if [[ "$OSTYPE" == darwin* ]]; then
   fi
 
   xcode-select --install >/dev/null 2>&1 || true
-fi
 
-if ! command -v brew >/dev/null 2>&1; then
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
-
-if [[ "$OSTYPE" == linux* ]]; then
+  install_homebrew_if_needed
+elif [[ "$OSTYPE" == linux* ]]; then
   sudo apt-get install build-essential curl file git
 
-  if ! [[ -d "$HOME/.linuxbrew" ]]; then
-    git clone https://github.com/Linuxbrew/brew.git ~/.linuxbrew
-    test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-    test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
-    test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
-    echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
-    export PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
-    export PATH="$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH"
-  fi
+  install_homebrew_if_needed
 fi
+
