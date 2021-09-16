bf535a49b0b6640c10bad977c01cfec5235c61dc
Faun <git@faun.me>
2021-09-15 17:12:03 -0700

    Improve base16 loading

--- shrc/03_base16.sh
+++ shrc/03_base16.sh
@@ -1,4 +1,15 @@
-BASE16_SHELL=$HOME/.config/base16-shell/
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+#!/usr/bin/env bash
 
-record_time "base16"
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:=${HOME}/.config}"
+
+BASE16_SHELL_PATH="${XDG_CONFIG_HOME}/base16-shell"
+
+# Load profile helpers
+[ -n "$PS1" ] && [ -s "${BASE16_SHELL_PATH}/profile_helper.sh" ] && eval "$("${BASE16_SHELL_PATH}/profile_helper.sh")"
+record_time "base16-profile-helper"
+
+# Load base16-shell
+# shellcheck disable=1091
+source "${BASE16_SHELL_PATH}/scripts/base16-twilight.sh"
+
+record_time "base16-shell"
