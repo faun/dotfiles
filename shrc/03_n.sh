d252eb4155cc8aeb42d7e96580814df23821a07d
Faun <git@faun.me>
2022-05-10 14:05:35 -0700

    Fix n path

--- shrc/03_n.sh
+++ shrc/03_n.sh
@@ -3,5 +3,7 @@
 N_PREFIX="${N_PREFIX:-$HOME/n}"
 if [[ -d "$N_PREFIX" ]]; then
   export N_PREFIX
-  [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
+  PATH="$N_PREFIX/bin:$PATH"
+  export PATH
 fi
+record_time "n node version manager"
