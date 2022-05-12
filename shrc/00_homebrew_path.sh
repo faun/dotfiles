74aa9a29f524dbc5d397497ae80c18289899c58d
Faun <git@faun.me>
2022-05-12 22:55:32 +0000

    Add linux brew path configuration

--- shrc/00_homebrew_path.sh
+++ shrc/00_homebrew_path.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if [[ -d "/home/linuxbrew/.linuxbrew" ]]; then
+  export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+
+  if type brew &>/dev/null; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+    PATH="$HOMEBREW_PREFIX:$PATH"
+  fi
+fi
 HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix)}"
 export HOMEBREW_PREFIX
 record_time "homebrew prefix"
