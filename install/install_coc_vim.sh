e81ac950a000da9165fbc327b0a491fd2c1dd2ed
Faun <git@faun.me>
2022-06-16 12:06:21 -0700

    Install additional extensions

--- install/install_coc_vim.sh
+++ install/install_coc_vim.sh
@@ -6,7 +6,7 @@ mkdir -p "$HOME/.local/share/nvim/site/pack/coc/start"
 cd "$HOME/.local/share/nvim/site/pack/coc/start"
 curl --fail -sSL https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
 
-nvim +:CocInstall coc-json coc-tsserver
+nvim +:"CocInstall coc-json coc-tsserver coc-sh coc-snippets coc-prettier coc-eslint coc-pairs" +qa
 
 # Install extensions
 mkdir -p "$HOME/.config/coc/extensions"
