67715cda7928354c71d9f53f2155b59e8f9b8389
Faun <git@faun.me>
2022-06-16 16:33:26 -0700

    Improve coc installer

--- install/install_coc_vim.sh
+++ install/install_coc_vim.sh
@@ -2,16 +2,18 @@
 
 set -eou pipefail
 
-mkdir -p "$HOME/.local/share/nvim/site/pack/coc/start"
-cd "$HOME/.local/share/nvim/site/pack/coc/start"
-curl --fail -sSL https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+if ! [[ -d "$HOME/.local/share/nvim/site/pack/coc/start" ]]; then
+  mkdir -p "$HOME/.local/share/nvim/site/pack/coc/start"
+  cd "$HOME/.local/share/nvim/site/pack/coc/start"
+  curl --fail -sSL https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
+fi
 
-nvim -U none +:"CocInstall coc-json coc-tsserver coc-sh coc-snippets coc-prettier coc-eslint coc-pairs" +qa
+nvim "+PlugUpdate" "+qa!" > /dev/null 2>&1
+nvim "+CocUpdate" "+qa!"
 
 # Install extensions
 mkdir -p "$HOME/.config/coc/extensions"
 cd "$HOME/.config/coc/extensions"
-if [ ! -f package.json ]
-then
-  echo '{"dependencies":{}}'> package.json
+if [ ! -f package.json ]; then
+  echo '{"dependencies":{}}' > package.json
 fi
