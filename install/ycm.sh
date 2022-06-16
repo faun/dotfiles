02ad345af26e3853d0ed80b8ea5258e84d57bae7
Faun <git@faun.me>
2022-06-16 16:51:22 -0700

    Improve YCM installer

--- install/ycm.sh
+++ install/ycm.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
-cd "$HOME/.config/vim/plugged/YouCompleteMe" || exit
-export PYTHON_CONFIGURE_OPTS="--enable-framework"
+set -eou pipefail
+
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-${HOME}/.config}"
+if ! [[ -d "$XDG_CONFIG_HOME" ]]; then
+  echo "The path at $XDG_CONFIG_HOME does not exist" >&2
+  exit 1
+fi
+
+if [[ -d "${XDG_CONFIG_HOME}/vim/plugged/YouCompleteMe" ]]; then
+  YCM_INSTALL_PATH="${XDG_CONFIG_HOME}/vim/plugged/YouCompleteMe"
+elif [[ -d "${HOME}/.vim/plugged/YouCompleteMe" ]]; then
+  YCM_INSTALL_PATH="${HOME}/.vim/plugged/YouCompleteMe"
+else
+  echo "Please install YouCompleteMe and try again"
+  echo "Try running: vim +PlugInstall +qa"
+  exit 1
+fi
+
+cd "$YCM_INSTALL_PATH" || (
+  echo "Could not cd to $YCM_INSTALL_PATH"
+  exit 1
+)
 
 latest_python_version=$(
   set +u
@@ -13,24 +33,31 @@ latest_python_version=$(
   set -u
 )
 
-if command -v pyenv >/dev/null 2>&1; then
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-
-  pyenv install -s "$latest_python_version"
-  pyenv virtualenv "$latest_python_version" py3ycm
-  pyenv activate "$latest_python_version/envs/py3ycm"
-
-  PYTHON3_PATH="$(pyenv which python)"
-else
-  PYTHON3_PATH="$(which python3)"
-fi
-
-FORMULAE=(llvm cmake)
+set +u
+FORMULAE=(llvm cmake openssl readline sqlite3 xz zlib tcl-tk bzip2 python3)
 for FORMULA in "${FORMULAE[@]}"; do
   brew info "$FORMULA" >/dev/null 2>&1 || brew install "$FORMULA"
 done
-INSTALL_COMMAND=("$PYTHON3_PATH" "install.py" "--go-completer")
+
+# if command -v pyenv >/dev/null 2>&1; then
+#   eval "$(pyenv init -)"
+#   eval "$(pyenv virtualenv-init -)"
+#   # LDFLAGS="-L$(brew --prefix zlib)/lib -L$(brew --prefix bzip2)/lib" \
+#   # CPPFLAGS="-I$(brew --prefix zlib)/include -I$(brew --prefix bzip2)/include" \
+#   # CONFIGURE_OPTS="--with-openssl=$(brew --prefix openssl)" \
+#   # PYTHON_CONFIGURE_OPTS="--enable-shared" \
+#   pyenv install -s "$latest_python_version"
+
+#   pyenv virtualenv "$latest_python_version" py3ycm
+#   pyenv activate "$latest_python_version/envs/py3ycm"
+
+#   PYTHON3_PATH="$(pyenv which python)"
+# else
+# fi
+PYTHON3_PATH="$(which python3)"
+
+INSTALL_COMMAND=("$PYTHON3_PATH" "install.py" "--go-completer" "--ts-completer")
+set -u
 
 echo "Running ${INSTALL_COMMAND[*]}"
 
