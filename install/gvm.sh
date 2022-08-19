f4ed4a14706d01896d71307fbbeb01d039649992
Faun <git@faun.me>
2022-08-19 11:43:21 -0700

    Update gvm installer

--- install/gvm.sh
+++ install/gvm.sh
@@ -3,14 +3,14 @@ set -eou pipefail
 
 GVM_HOME="${GVM_HOME:-$HOME/.gvm}"
 if ! [[ -d "$GVM_HOME" ]]; then
-  xcode-select --install >/dev/null 2>&1 || true
-  brew update || true
-  brew install mercurial || true
+	xcode-select --install >/dev/null 2>&1 || true
+	brew update || true
+	brew install mercurial || true
 
-  bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-  gvm use system --default
+	bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+	gvm use system --default
 else
-  echo "GVM is already installed, updating"
-  gvm update
+	echo "GVM is already installed, updating"
+	gvm update
 fi
 gvm listall | grep -E "   go" | grep -vE "beta|rc" | tail -n 20 | fzf --header-first --tac --no-sort --header="Select a Go version to install" | xargs gvm install
