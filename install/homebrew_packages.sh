cfa7152f61e4189cc5752fde4b2ee60cb90d9920
Faun <git@faun.me>
2022-08-25 08:54:31 -0700

    Fix missing variable

--- install/homebrew_packages.sh
+++ install/homebrew_packages.sh
@@ -3,36 +3,36 @@
 set -e
 
 homebrew_dependencies=(
-  antibody
-  the_silver_searcher
-  neovim
-  fasd
-  tmux
-  shfmt
-  zsh-autosuggestions
-  universal-ctags
-  mas
-  pyenv-virtualenv
-  bat
-  fzf
-  watchman
-  yoheimuta/protolint/protolint
+	antibody
+	the_silver_searcher
+	neovim
+	fasd
+	tmux
+	shfmt
+	zsh-autosuggestions
+	universal-ctags
+	mas
+	pyenv-virtualenv
+	bat
+	fzf
+	watchman
+	yoheimuta/protolint/protolint
 )
 
 for brew_package in "${homebrew_dependencies[@]}"; do
-  if ! brew ls --versions | awk '{ print $1 }' | grep "^$brew_package\$" > /dev/null; then
-    echo "Installing package: $brew_package"
-    set +e
-    brew install "$brew_package" 2> /tmp/package_error
-    status=$?
-    set -e
-    if [[ status != 0 ]]; then
-      echo "Package $brew_package failed to install!"
-      echo ---
-      cat /tmp/package_error
-      echo ---
-    fi
-  else
-    echo "Package $brew_package already installed"
-  fi
+	if ! brew ls --versions | awk '{ print $1 }' | grep "^$brew_package\$" >/dev/null; then
+		echo "Installing package: $brew_package"
+		set +e
+		brew install "$brew_package" 2>/tmp/package_error
+		status=$?
+		set -e
+		if [[ $status != 0 ]]; then
+			echo "Package $brew_package failed to install!"
+			echo ---
+			cat /tmp/package_error
+			echo ---
+		fi
+	else
+		echo "Package $brew_package already installed"
+	fi
 done
