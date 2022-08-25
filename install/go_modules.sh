cc36429998ca2312ff3349bb0c46f030bba4bad4
Faun <git@faun.me>
2022-08-25 09:02:41 -0700

    Add global go module installer

---
+++ install/go_modules.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+go_modules=(
+	github.com/nametake/golangci-lint-langserver
+)
+
+for module in "${go_modules[@]}"; do
+	echo "Installing module: $module"
+	set +e
+	go get "$module" 2>/tmp/go_module_error
+	status=$?
+	set -e
+	if [[ $status != 0 ]]; then
+		echo "Package $module failed to install!"
+		echo ---
+		cat /tmp/go_module_error
+		echo ---
+	fi
+done
