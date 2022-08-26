718ad0b9f4944b2782bee66f640629c61eaf8cc9
Faun <git@faun.me>
2022-08-26 09:11:47 -0700

    Shorten GCP startup time

--- shrc/03_google-cloud-sdk.sh
+++ shrc/03_google-cloud-sdk.sh
@@ -1,11 +1,19 @@
-if type brew &>/dev/null; then
-  if [[ -d "${HOMEBREW_PREFIX:?}/Caskroom/google-cloud-sdk/latest/" ]]; then
-    if [[ -n $ZSH_NAME ]]; then
-      source "${HOMEBREW_PREFIX:?}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
-      source "${HOMEBREW_PREFIX:?}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
-    else
-      source "${HOMEBREW_PREFIX:?}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
-      source "${HOMEBREW_PREFIX:?}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
-    fi
-  fi
+#!/usr/bin/env bash
+# shellcheck disable=SC1090
+
+HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-}"
+if [[ "$HOMEBREW_PREFIX" == "" ]]; then
+	if type brew &>/dev/null; then
+		HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix)}"
+	fi
+fi
+
+if [[ -d "${HOMEBREW_PREFIX:?}/Caskroom/google-cloud-sdk/latest/" ]]; then
+	if [[ -n $ZSH_NAME ]]; then
+		source "${HOMEBREW_PREFIX:?}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+		source "${HOMEBREW_PREFIX:?}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+	else
+		source "${HOMEBREW_PREFIX:?}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
+		source "${HOMEBREW_PREFIX:?}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
+	fi
 fi
