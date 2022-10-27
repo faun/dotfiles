13cc26fcf191a2574db5caf4ae3555eb1f346a2d
Faun <git@faun.me>
2022-10-27 09:33:01 -0700

    Fix Homebrew bin path declaration

--- shrc/00_homebrew_path.sh
+++ shrc/00_homebrew_path.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [[ -d "/home/linuxbrew/.linuxbrew" ]]; then
-  export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+	export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
 
-  if type brew &>/dev/null; then
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    PATH="$HOMEBREW_PREFIX:$PATH"
-  fi
+	if type brew &>/dev/null; then
+		eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+		PATH="${HOMEBREW_PREFIX}/bin:$PATH"
+	fi
 fi
 HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix)}"
 export HOMEBREW_PREFIX
