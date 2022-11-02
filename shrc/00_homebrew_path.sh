6f8c9159f6dfb32fa566958eb609adea9ddb353d
Faun <git@faun.me>
2022-11-02 10:03:03 -0700

    Only add $HOMEBREW_PREFIX/bin to path if defined

--- shrc/00_homebrew_path.sh
+++ shrc/00_homebrew_path.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+
 if [[ -d "/home/linuxbrew/.linuxbrew" ]]; then
 	export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
 
 	if type brew &>/dev/null; then
 		eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-		PATH="${HOMEBREW_PREFIX}/bin:$PATH"
+		if [[ -n "$HOMEBREW_PREFIX" ]]; then
+      PATH="${HOMEBREW_PREFIX:?}/bin:$PATH"
+    fi
 	fi
 fi
+
 HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix)}"
 export HOMEBREW_PREFIX
 record_time "homebrew prefix"
