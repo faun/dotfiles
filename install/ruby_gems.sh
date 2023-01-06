c87ad407818b6919e63e75b4e194a90e81c0af56
Faun <git@faun.me>
2023-01-06 10:22:04 -0800

    Install gems with rbenv

--- install/ruby_gems.sh
+++ install/ruby_gems.sh
@@ -4,8 +4,8 @@ set -eou pipefail
 cd "$(dirname "$0")" || exit 1
 # DIR="$(pwd)"
 
-rubygems_packages=(neovim scss_lint)
+rubygems_packages=(neovim)
 for gem in "${rubygems_packages[@]}"; do
-  echo "Installing gem: $gem"
-  rvm "@global do gem install $gem" 2>/dev/null || gem install "$gem"
+	echo "Installing gem: $gem"
+	rbenv each gem install "$gem" 2>/dev/null || gem install "$gem"
 done
