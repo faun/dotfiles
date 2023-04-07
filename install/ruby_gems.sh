f4cc110848104cdf34a015eb57e4eeeb0cbbce86
Faun <git@faun.me>
2023-04-07 08:59:05 -0700

    Add ripper-tags

--- install/ruby_gems.sh
+++ install/ruby_gems.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 cd "$(dirname "$0")" || exit 1
 # DIR="$(pwd)"
 
-rubygems_packages=(neovim)
+rubygems_packages=(neovim ripper-tags)
 for gem in "${rubygems_packages[@]}"; do
 	echo "Installing gem: $gem"
 	rbenv each gem install "$gem" 2>/dev/null || gem install "$gem"
