59162e37f5c210e509d75b97dcaa2c6916396946
Faun <git@faun.me>
2023-05-24 14:57:15 -0700

    Add install script output

--- install/install_coc_vim.sh
+++ install/install_coc_vim.sh
@@ -9,13 +9,15 @@ if ! [[ -d "$HOME/.local/share/nvim/site/pack/coc/start" ]]; then
 	curl --fail -sSL https://github.com/neoclide/coc.nvim/archive/release.tar.gz | tar xzfv -
 fi
 
+echo "Updating coc.nvim..."
 nvim "+PlugUpdate" "+qa!" >/dev/null 2>&1
+echo "Updating coc.nvim extensions..."
 nvim "+CocUpdate" "+qa!"
 
-# Install extensions
 echo "Installing extensions"
 mkdir -p "$HOME/.config/coc/extensions"
 cd "$HOME/.config/coc/extensions"
 if [ ! -f package.json ]; then
 	echo '{"dependencies":{}}' >package.json
 fi
+echo "Done"
