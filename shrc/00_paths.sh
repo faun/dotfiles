cf5ae83c5f021a7a31ead1cec8ec90dbde412d39
Faun <git@faun.me>
2023-07-11 11:00:56 -0700

    Improve paths

--- shrc/00_paths.sh
+++ shrc/00_paths.sh
@@ -4,18 +4,61 @@ if [[ -z $TMUX ]]; then
   export GOPATH="$HOME/src"
   export GOBIN="$HOME/bin"
 
-  PATH="$PATH:/usr/local/mysql/bin"
-  PATH="$PATH:/usr/local/git/bin"
-  PATH="$PATH:$HOME/.cargo/bin"
-  PATH="$PATH:$HOME/.local/bin"
-  PATH="$HOME/.yarn/bin:$PATH"
-  PATH="$HOME/.config/kubectx:$PATH"
-  PATH="/usr/local/go/bin:$GOPATH/bin:$PATH"
+  if [[ -d "/usr/local/mysql/bin" ]]; then
+    PATH="$PATH:/usr/local/mysql/bin"
+  fi
+
+  if [[ -d "/usr/local/git/bin" ]]; then
+    PATH="$PATH:/usr/local/git/bin"
+  fi
+
+  if [[ -d "$HOME/.cargo/bin" ]]; then
+    PATH="$PATH:$HOME/.cargo/bin"
+  fi
+
+  if [[ -d "$HOME/.local/bin" ]]; then
+    PATH="$PATH:$HOME/.local/bin"
+  fi
+
+  if [[ -d "$HOME/.yarn/bin" ]]; then
+    PATH="$PATH:$HOME/.yarn/bin"
+  fi
+
   RBENV_ROOT="${RBENV_ROOT:-$HOME/.rbenv}"
   if [ -d "$RBENV_ROOT" ]; then
     PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"
   fi
-  PATH="$HOME/.pyenv/shims:$PATH"
+
+  PYENV_ROOT="${PYENV_ROOT:-$HOME/.pyenv}"
+  if [[ -d "$PYENV_ROOT" ]]; then
+    PATH="$HOME/.pyenv/bin:$PATH"
+    PATH="$HOME/.pyenv/shims:$PATH"
+  fi
+
+  if [[ -d "$HOME/.config/kubectx" ]]; then
+    PATH="$HOME/.config/kubectx:$PATH"
+  fi
+
+  if [[ -d "/usr/local/go/bin" ]]; then
+    PATH="$PATH:/usr/local/go/bin"
+  fi
+
+  if [[ -z $GOPATH ]]; then
+    GOPATH="$HOME/src"
+  fi
+
+  if [[ -z $GOBIN ]]; then
+    GOBIN="$HOME/bin"
+  fi
+
+  if [[ -d "$GOBIN" ]]; then
+    PATH="$PATH:$GOBIN"
+  fi
+
+  if [[ -d "$GOPATH/bin" ]]; then
+    PATH="$PATH:$GOPATH/bin"
+  fi
+
   PATH=".git/safe/../../bin:$PATH"
   PATH=".git/safe/../../node_modules/.bin:$PATH"
   PATH="$HOME/bin:$PATH"
