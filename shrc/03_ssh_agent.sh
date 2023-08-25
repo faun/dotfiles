c240872a651c20bc21cb1ca520dd33a0cbddc723
Faun <git@faun.me>
2023-08-25 10:29:45 -0700

    Don't assume FIDO authenticator

--- shrc/03_ssh_agent.sh
+++ shrc/03_ssh_agent.sh
@@ -1,24 +1,21 @@
 SSH_ENV="$HOME/.ssh/environment"
 
 function start_agent {
-  /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
-  chmod 600 "${SSH_ENV}"
-  source "${SSH_ENV}" > /dev/null
-  # Note this only works on macOS Sierra +
-  ssh-add -K ~/.ssh/id_rsa &> /dev/null
-  ssh-add -A &> /dev/null
+	/usr/bin/ssh-agent | sed 's/^echo/#echo/' >"${SSH_ENV}"
+	chmod 600 "${SSH_ENV}"
+	source "${SSH_ENV}" >/dev/null
+	ssh-add -A &>/dev/null
 }
 
-if [[ -d "$HOME/.ssh" ]]
-then
-  # Source SSH settings, if applicable
+if [[ -d "$HOME/.ssh" ]]; then
+	# Source SSH settings, if applicable
 
-  if [ -f "${SSH_ENV}" ]; then
-    source "${SSH_ENV}" > /dev/null
-    ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
-    start_agent;
-  }
-  else
-    start_agent;
-  fi
+	if [ -f "${SSH_ENV}" ]; then
+		source "${SSH_ENV}" >/dev/null
+		ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ >/dev/null || {
+			start_agent
+		}
+	else
+		start_agent
+	fi
 fi
