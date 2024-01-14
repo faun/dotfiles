da4b1df86bd5aca6e6081a7d3b6fa5713bec47f6
Faun <git@faun.me>
2024-01-13 16:29:49 -0800

    Set locale environment variables

--- shrc/01_config.sh
+++ shrc/01_config.sh
@@ -1,6 +1,9 @@
 # Ensure correct locale
+export LANGUAGE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_CTYPE=en_US.UTF-8
+
 export EDITOR='nvim'
 [ -z "$TMUX" ] && export TERM=xterm-256color
 
