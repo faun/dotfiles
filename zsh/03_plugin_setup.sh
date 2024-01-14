b2c10a6bf206f13f40ca419d24fa10a482341d0e
Faun <git@faun.me>
2024-01-13 16:28:03 -0800

    Update plugin setup

--- zsh/03_plugin_setup.sh
+++ zsh/03_plugin_setup.sh
@@ -1,38 +1,50 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
 
-configure_antibody() {
-  record_time "antibody autoload"
+configure_antidote() {
+	record_time "antidote autoload"
 
-  source <(antibody init)
-  record_time "antibody init"
+	source <(antidote init)
+	record_time "antidote init"
 
-  antibody_packages=(
-    zsh-users/zsh-syntax-highlighting
-    zsh-users/zsh-history-substring-search
-    lukechilds/zsh-better-npm-completion
-  )
+	ANTIDOTE_PACKAGES="$(
+		cat <<EOF
+		zsh-users/zsh-syntax-highlighting
+		zsh-users/zsh-autosuggestions
+		zsh-users/zsh-history-substring-search
+		lukechilds/zsh-better-npm-completion
+EOF
+	)"
+	ANTIDOTE_PLUGINS_PATH="${ZDOTDIR:-$HOME}/.zsh_plugins.txt"
+	if ! [[ -f "${ANTIDOTE_PLUGINS_PATH:?}" ]]; then
+		echo "${ANTIDOTE_PACKAGES:?}" >"${ANTIDOTE_PLUGINS_PATH:?}"
+	fi
+	antidote load "${ANTIDOTE_PLUGINS_PATH:?}"
+	record_time "antidote plugins"
 
-  for package in "${antibody_packages[@]}"; do
-    antibody bundle "$package"
-    record_time "$package"
-  done
-  record_time "antibody bundles"
+	bindkey '^[[A' history-substring-search-up
+	bindkey '^[[B' history-substring-search-down
 
-  bindkey '^[[A' history-substring-search-up
-  bindkey '^[[B' history-substring-search-down
+	bindkey -M emacs '^P' history-substring-search-up
+	bindkey -M emacs '^N' history-substring-search-down
 
-  bindkey -M emacs '^P' history-substring-search-up
-  bindkey -M emacs '^N' history-substring-search-down
+	bindkey -M vicmd 'k' history-substring-search-up
+	bindkey -M vicmd 'j' history-substring-search-down
 
-  bindkey -M vicmd 'k' history-substring-search-up
-  bindkey -M vicmd 'j' history-substring-search-down
-
-  record_time "antibody keybindings"
+	record_time "antidote keybindings"
 }
 
-if command -v antibody >/dev/null 2>&1; then
-  configure_antibody
+ANTIDOTE_PATH="${ZDOTDIR:-$HOME}/.antidote"
+if ! [[ -d "$ANTIDOTE_PATH" ]]; then
+	git clone --depth=1 https://github.com/mattmc3/antidote.git "${ANTIDOTE_PATH:?}" || true
+fi
+
+if [[ -d "${ANTIDOTE_PATH:?}" ]]; then
+	source "${ANTIDOTE_PATH:?}/antidote.zsh"
+fi
+
+if command -v antidote >/dev/null 2>&1; then
+	configure_antidote
 else
-  echo "Couldn't find antibody binary"
+	echo "Couldn't find antidote binary"
 fi
