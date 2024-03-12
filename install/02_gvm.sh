95c32b496b0a4e8cdb168ab415b00f028c01e43c
Faun <git@faun.me>
2024-03-12 14:51:32 -0700

    Install gvm version if not installed

--- install/02_gvm.sh
+++ install/02_gvm.sh
@@ -19,4 +19,11 @@ else
 	echo "GVM is already installed, updating"
 	gvm update || true
 fi
-gvm listall | grep -E "   go" | grep -vE "beta|rc" | tail -n 20 | fzf --header-first --tac --no-sort --header="Select a Go version to install" | xargs gvm install
+
+INSTALLED_GO_VERSIONS="$(
+	gvm list | sed '/^[[:space:]]*$/d' | awk '{$1=$1};1' | grep -vE '^gvm' | wc -l
+)"
+
+if [[ $INSTALLED_GO_VERSIONS -eq 0 ]]; then
+	gvm listall | grep -E "   go" | grep -vE "beta|rc" | tail -n 20 | fzf --header-first --tac --no-sort --header="Select a Go version to install" | xargs gvm install
+fi
