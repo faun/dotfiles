2d570ce720ab6ffb29ee506b80c857e61927de8b
Faun <git@faun.me>
2024-03-13 11:14:51 -0700

    Enable treesitter indent

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -16,6 +16,8 @@ require('nvim-treesitter.configs').setup {
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
+  indent = { enable = true },
+
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
