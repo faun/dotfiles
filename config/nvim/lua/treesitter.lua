0cc7c3b2e7c044d1c63167c366201de11d09f326
Faun <git@faun.me>
2024-03-13 11:14:36 -0700

    Install all parsers

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -1,7 +1,7 @@
 require('nvim-treesitter.configs').setup {
   -- A list of parser names, or "all"
   --
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'cmake', 'ruby' },
+  ensure_installed = "all"
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
