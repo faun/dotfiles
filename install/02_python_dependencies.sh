eadb15f5e933138c867e51ec92f3758b17cf2531
Faun <git@faun.me>
2024-03-12 17:21:19 -0700

    Improve python installer

--- install/02_python_dependencies.sh
+++ install/02_python_dependencies.sh
@@ -8,26 +8,21 @@ cd "$(dirname "$0")" || exit 1
 # https://github.com/zchee/deoplete-jedi/wiki/Setting-up-Python-for-Neovim
 
 if [[ -d "$HOME/.pyenv/plugins/python-build/../.." ]]; then
-  echo "Updating python-build"
-  cd "$HOME/.pyenv/plugins/python-build/../.." && git pull && cd -
-fi
-
-if command -v pip >/dev/null 2>&1; then
-  pip install --upgrade pip
+	echo "Updating python-build"
+	cd "$HOME/.pyenv/plugins/python-build/../.." && git pull && cd -
 fi
 
 if ! brew ls --versions | awk '{ print $1 }' | grep 'pyenv$' >/dev/null; then
-  echo "Installing pyenv"
-  brew install pyenv
+	echo "Installing pyenv"
+	brew install pyenv
 fi
 
 if ! brew ls --versions | awk '{ print $1 }' | grep 'readline' >/dev/null; then
-  echo "Installing readline"
-  brew install readline
+	echo "Installing readline"
+	brew install readline
 fi
 
 if ! brew ls --versions | awk '{ print $1 }' | grep 'xz' >/dev/null; then
-  echo "Installing xz"
-  brew install xz
+	echo "Installing xz"
+	brew install xz
 fi
-
--- install/03_python2.sh
+++ install/03_python2.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 cd "$(dirname "$0")" || exit 1
 DIR="$(pwd)"
 
-"${DIR}/python_dependencies.sh"
+"${DIR}/02_python_dependencies.sh"
 set +u
 export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 eval "$(pyenv init -)"
@@ -16,29 +16,29 @@ set -u
 installed_python2_version="$(python2 --version 2>/dev/null | sed 's/Python //')"
 
 latest_python_2_version=$(
-  pyenv install --list |
-    sed 's/^  //' |
-    grep '^2\.' |
-    grep --invert-match 'dev\|a\|b\|rc' |
-    tail -1
+	pyenv install --list |
+		sed 's/^  //' |
+		grep '^2\.' |
+		grep --invert-match 'dev\|a\|b\|rc' |
+		tail -1
 )
 
 echo "Latest python version: $latest_python_2_version"
 echo "Installed python version: $installed_python2_version"
 
 if [[ "$installed_python2_version" != "$latest_python_2_version" ]]; then
-  echo "Installing Python $latest_python_2_version"
-  pyenv install -s "$latest_python_2_version"
+	echo "Installing Python $latest_python_2_version"
+	pyenv install -s "$latest_python_2_version"
 
-  pyenv global "$latest_python_2_version"
+	pyenv global "$latest_python_2_version"
 
-  echo "Installing neovim for Python2"
-  pip2 install --upgrade --force-reinstall pip setuptools wheel neovim
+	echo "Installing neovim for Python2"
+	pip2 install --upgrade --force-reinstall pip setuptools wheel neovim
 
-  if ! grep "g:python_host_prog" "$HOME/.vimrc.local" >/dev/null; then
-    echo "Adding Neovim configuration for Python2"
-    echo "let g:python_host_prog = '$(which python2)'" >>"$HOME/.vimrc.local"
-  fi
+	if ! grep "g:python_host_prog" "$HOME/.vimrc.local" >/dev/null; then
+		echo "Adding Neovim configuration for Python2"
+		echo "let g:python_host_prog = '$(which python2)'" >>"$HOME/.vimrc.local"
+	fi
 fi
 
 # -----------------------------------------------------------------------------
@@ -51,9 +51,9 @@ set +u
 
 virtualenv_destination="$(pyenv root)/plugins/pyenv-virtualenv"
 if ! [[ -d "$virtualenv_destination" ]]; then
-  git clone https://github.com/pyenv/pyenv-virtualenv.git "$virtualenv_destination"
+	git clone https://github.com/pyenv/pyenv-virtualenv.git "$virtualenv_destination"
 else
-  (cd "$virtualenv_destination" && git reset --hard origin/master)
+	(cd "$virtualenv_destination" && git reset --hard origin/master)
 fi
 
 pyenv virtualenv "$latest_python_2_version" py2neovim
@@ -63,18 +63,18 @@ PYTHON2_PATH="$(pyenv which python2)"
 echo "$PYTHON2_PATH"
 
 if ! grep "g:python_host_prog" "$HOME/.vimrc.local" >/dev/null; then
-  pip install --upgrade --force-reinstall pip setuptools wheel neovim >/dev/null
+	pip2 install --upgrade --force-reinstall pip setuptools wheel neovim >/dev/null
 
-  echo "Adding Neovim configuration for Python2"
-  echo "let g:python_host_prog=\"$PYTHON2_PATH\"" >>"$HOME/.vimrc.local"
+	echo "Adding Neovim configuration for Python2"
+	echo "let g:python_host_prog=\"$PYTHON2_PATH\"" >>"$HOME/.vimrc.local"
 else
-  echo "Please remove g:python_host_prog from $HOME/.vimrc.local and retry"
+	echo "Please remove g:python_host_prog from $HOME/.vimrc.local and retry"
 fi
 
 # -----------------------------------------------------------------------------
 
 pip_packages=(yamllint)
 for egg in "${pip_packages[@]}"; do
-  echo "Installing egg: $egg"
-  pip install "$egg"
+	echo "Installing egg: $egg"
+	pip2 install "$egg"
 done
