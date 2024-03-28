bd1e1c3756c93e0bc91df79e407f88fe65b17e9f
Faun <git@faun.me>
2024-03-27 17:04:34 -0700

    Fix formatting

--- config/nvim/lua/plugins/spec/linting.lua
+++ config/nvim/lua/plugins/spec/linting.lua
@@ -1,11 +1,9 @@
 return {
-  {
-    -- https://github.com/stevearc/conform.nvim
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    config = function()
-      require("plugins.config.format").setup_conform()
-    end,
-  },
+  -- https://github.com/stevearc/conform.nvim
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  config = function()
+    require("plugins.config.format").setup_conform()
+  end,
 }
