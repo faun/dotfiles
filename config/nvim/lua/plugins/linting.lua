064ac24765ea870363e28db77484031b1fa6aa14
Faun <git@faun.me>
2024-03-27 17:39:52 -0700

    Remove linting configuration

--- config/nvim/lua/plugins/linting.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- https://github.com/stevearc/conform.nvim
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  config = function()
-    require("plugins.config.format").setup_conform()
-  end,
-}
