781976beab31fc5c8b59084c77627035f34acd0f
Faun <git@faun.me>
2024-03-29 16:36:14 -0700

    Update testing config

--- config/nvim/lua/plugins/testing.lua
+++ config/nvim/lua/plugins/testing.lua
@@ -1,35 +1,48 @@
 return {
-  "nvim-neotest/neotest",
-  lazy = true,
-  dependencies = {
-    "antoinemadec/FixCursorHold.nvim",
-    "nvim-lua/plenary.nvim",
-    "nvim-neotest/neotest-go",
-    "nvim-neotest/neotest-plenary",
-    "nvim-neotest/nvim-nio",
-    "nvim-treesitter/nvim-treesitter",
-    "olimorris/neotest-rspec",
-    "preservim/vimux",
-    "vim-test/vim-test",
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "vim-test/vim-test",
+      "preservim/vimux",
+      "nvim-neotest/neotest-vim-test",
+      "nvim-neotest/neotest-go",
+      "olimorris/neotest-rspec",
+    },
+    opts = {
+      adapters = function()
+        return {
+          require("neotest-go"),
+          require("neotest-rspec"),
+          require("neotest-vim-test")({
+            ignore_filetypes = {},
+          }),
+        }
+      end,
+    },
   },
-  config = function()
-    require("neotest").setup({
-      adapters = {
-        require("neotest-go")({
-          experimental = {
-            test_table = true,
-          },
-          args = { "-count=1", "-timeout=60s" },
-        }),
-        require("neotest-rspec"),
-        require("neotest-plenary"),
-        require("neotest-vim-test")({
-          ignore_file_types = { "vim", "lua" },
-        }),
-      },
-    })
-    require("neodev").setup({
-      library = { plugins = { "neotest" }, types = true },
-    })
-  end,
+  {
+    "vim-test/vim-test",
+    dependencies = {
+      "preservim/vimux",
+    },
+    keys = {
+      { "<leader><CR>", "<cmd>TestNearest<cr>", desc = "Test nearest" },
+      { "<leader>tn", "<cmd>TestNearest<cr>", desc = "Test file" },
+      { "<leader>\\", "<cmd>TestFile<cr>", desc = "Test file" },
+      { "<leader>tf", "<cmd>TestFile<cr>", desc = "Test file" },
+      { "<leader>ts", "<cmd>TestSuite<CR>", desc = "Test suite" },
+      { "<leader>tl", "<cmd>TestLast<CR>", desc = "Test last" },
+      { "<leader>tv", "<cmd>TestVisit<CR>", desc = "Test visit" },
+    },
+    init = function()
+      if os.getenv("TMUX") ~= "" then
+        vim.g["test#strategy"] = "vimux"
+        vim.g["test#preserve_screen"] = 0
+        vim.g.VimuxOrientation = "v"
+        vim.g.VimuxHeight = "40"
+      end
+    end,
+  },
 }
