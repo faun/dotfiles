597fd1870ef0af8f6b607064afe85b3b15ebb5b7
Faun <git@faun.me>
2024-03-29 16:53:58 -0700

    Move testing code

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -93,6 +93,56 @@ require("lazy").setup({
         },
       },
     },
+    {
+      "nvim-neotest/neotest",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "vim-test/vim-test",
+        "preservim/vimux",
+        "nvim-neotest/neotest-vim-test",
+        "nvim-neotest/neotest-go",
+        "olimorris/neotest-rspec",
+      },
+      opts = {
+        adapters = function()
+          local neotest_go = require("neotest-go")
+          local neotest_rspec = require("neotest-rspec")
+          local neotest_vim_test = require("neotest-vim-test")
+
+          return {
+            neotest_go,
+            neotest_rspec,
+            neotest_vim_test({
+              ignore_filetypes = {},
+            }),
+          }
+        end,
+      },
+    },
+    {
+      "vim-test/vim-test",
+      dependencies = {
+        "preservim/vimux",
+      },
+      keys = {
+        { "<leader><CR>", "<cmd>TestNearest<cr>", desc = "Test nearest" },
+        { "<leader>tn", "<cmd>TestNearest<cr>", desc = "Test file" },
+        { "<leader>\\", "<cmd>TestFile<cr>", desc = "Test file" },
+        { "<leader>tf", "<cmd>TestFile<cr>", desc = "Test file" },
+        { "<leader>ts", "<cmd>TestSuite<CR>", desc = "Test suite" },
+        { "<leader>tl", "<cmd>TestLast<CR>", desc = "Test last" },
+        { "<leader>tv", "<cmd>TestVisit<CR>", desc = "Test visit" },
+      },
+      init = function()
+        if os.getenv("TMUX") ~= "" then
+          vim.g["test#strategy"] = "vimux"
+          vim.g["test#preserve_screen"] = 0
+          vim.g.VimuxOrientation = "v"
+          vim.g.VimuxHeight = "40"
+        end
+      end,
+    },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.ruby" },
