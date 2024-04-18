cf173556e18c11c3334420a0d2af377c456171cd
Faun <git@faun.me>
2024-04-18 15:58:20 -0700

    Add nvim-lint config

---
+++ config/nvim/lua/plugins/linting.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    event = "LazyFile",
+    opts = {
+      events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+      linters_by_ft = {
+        css = { "stylelint" },
+        fish = { "fish" },
+        go = { "golangcilint" },
+        html = { "htmlhint" },
+        json = { "jsonlint" },
+        less = { "stylelint" },
+        lua = { "luacheck" },
+        markdown = { "markdownlint" },
+        python = { "flake8" },
+        ruby = { "ruby", "rubocop" },
+        sass = { "stylelint" },
+        scss = { "stylelint" },
+        sh = { "shellcheck" },
+        vim = { "vint" },
+        vue = { "eslint" },
+        yaml = { "yamllint" },
+      },
+    },
+  },
+}
