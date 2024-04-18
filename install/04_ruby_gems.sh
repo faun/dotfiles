315264ac4d52eaadd9f696674a07829e51740b16
Faun <git@faun.me>
2024-04-18 15:57:20 -0700

    Install python linters

--- install/04_ruby_gems.sh
+++ install/04_ruby_gems.sh
@@ -5,11 +5,11 @@ cd "$(dirname "$0")" || exit 1
 # DIR="$(pwd)"
 
 rubygems_packages=(
-	neovim
-	ripper-tags
-	erb-formatter
+  neovim
+  ripper-tags
+  erb-formatter
 )
 for gem in "${rubygems_packages[@]}"; do
-	echo "Installing gem: $gem"
-	rbenv each gem install "$gem" 2>/dev/null || gem install "$gem"
+  echo "Installing gem: $gem"
+  rbenv each gem install "$gem" 2>/dev/null || gem install "$gem"
 done
