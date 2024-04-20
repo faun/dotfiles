4dc7e85ff0c66e53ad1c8a3554d103f5ec13495c
Faun <git@faun.me>
2024-04-19 21:19:46 -0700

    Add none-ls

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -45,6 +45,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.vscode" },
     { "tpope/vim-eunuch" },
--- config/nvim/lua/plugins/linting.lua
+++ config/nvim/lua/plugins/linting.lua
@@ -23,4 +23,13 @@ return {
       },
     },
   },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        nls.builtins.diagnostics.golangci_lint,
+      })
+    end,
+  },
 }
