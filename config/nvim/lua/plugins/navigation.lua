d3fa956cfc4da7f5b5ac438aa5d48e5491d80e4f
Faun <git@faun.me>
2024-04-29 16:48:47 -0700

    Replace vim-projectionist with other-nvim

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -1,19 +1,18 @@
 return {
-  "tpope/vim-projectionist",
+  "rgroli/other.nvim",
   config = function()
-    vim.cmd([[
-          let g:projectionist_heuristics ={
-          \  "spec/*.rb": {
-          \     "app/*.rb": { "alternate": "spec/{}_spec.rb", "type": "source"},
-          \     "lib/*.rb": { "alternate": "spec/{}_spec.rb", "type": "source"},
-          \     "spec/*_spec.rb": { "alternate": ["app/{}.rb","lib/{}.rb"],"type": "test"},
-          \  },
-          \ "*_test.go": {
-          \    "*.go":       { "alternate": "{}_test.go", "type": "test" },
-          \    "*_test.go":  { "alternate": "{}.go", "type": "source" },
-          \  },
-          \}
-        ]])
+    require("other-nvim").setup({
+      mappings = {
+        "rails",
+        "golang",
+      },
+    })
+
+    -- Create aliases for vim-projectionist commonly used commands
+    vim.api.nvim_create_user_command("AV", "OtherVSplit", {})
+    vim.api.nvim_create_user_command("AH", "OtherSplit", {})
+    vim.api.nvim_create_user_command("AT", "OtherTab", {})
+    vim.api.nvim_create_user_command("A", "Other", {})
   end,
   event = { "BufReadPost", "BufNewFile" },
 }
