63c44b1a6e320f2c0cef503f22e4b3ed5cff6d98
Faun <git@faun.me>
2024-05-15 11:56:18 -0700

    Install debugpy

--- install/03_python3.sh
+++ install/03_python3.sh
@@ -25,14 +25,14 @@ latest_python_version=$(
 		tail -1 2>/dev/null
 	set -u
 )
-echo "Latest python version: $latest_python_version"
-echo "Installed python version: $installed_python_3_version"
+echo "Latest python version: ${latest_python_version:?}"
+echo "Installed python version: ${installed_python_3_version:?}"
 
-if [[ "$installed_python_3_version" != "$latest_python_version" ]]; then
-	echo "Installing Python $latest_python_version"
+if [[ "${installed_python_3_version:?}" != "${latest_python_version:?}" ]]; then
+	echo "Installing Python ${latest_python_version:?}"
 	set +u
-	pyenv install -s "$latest_python_version"
-	pyenv global "$latest_python_version"
+	pyenv install -s "${latest_python_version:?}"
+	pyenv global "${latest_python_version:?}"
 	set -u
 fi
 
@@ -40,12 +40,12 @@ echo "Installing pip and neovim for Python3"
 
 set +u
 pyenv uninstall --force py3neovim
-pyenv virtualenv "$latest_python_version" py3neovim
+pyenv virtualenv "${latest_python_version:?}" py3neovim
 set -u
 
 PYTHON3_PATH="$(
 	set +u
-	pyenv activate "$latest_python_version/envs/py3neovim"
+	pyenv activate "${latest_python_version:?}/envs/py3neovim"
 	pyenv which python
 	set -u
 )"
@@ -62,6 +62,15 @@ if ! grep "g:python3_host_prog" "$HOME/.vimrc.local" >/dev/null; then
 else
 	echo "Please remove g:python3_host_prog from $HOME/.vimrc.local and retry"
 fi
+
+echo "Installing debugpy"
+
+set +u
+pyenv uninstall --force debugpy
+pyenv virtualenv "${latest_python_version:?}" debugpy
+pyenv activate "${latest_python_version:?}/envs/debugpy"
+pip3 install --upgrade debugpy >/dev/null
+set -u
 
 # -----------------------------------------------------------------------------
 # Install Python packages
--- shrc/03_pyenv.sh
+++ shrc/03_pyenv.sh
@@ -1,13 +1,13 @@
+#!/usr/bin/env bash
+
 # Set PYENV_ROOT to ~/.pyenv unless it exists
-if [[ "x$PYENV_ROOT" == "x" ]]; then
-  export PYENV_ROOT=$HOME/.pyenv
-fi
+PYENV_ROOT="${PYENV_ROOT:-"${HOME:?}/.pyenv"}"
 record_time "pyenv paths"
 
 if [ -d "$PYENV_ROOT" ]; then
-  if command -v pyenv >/dev/null 2>&1; then
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
-  fi
+	if command -v pyenv >/dev/null 2>&1; then
+		eval "$(pyenv init -)"
+		eval "$(pyenv virtualenv-init -)"
+	fi
 fi
 record_time "pyenv initialization"
