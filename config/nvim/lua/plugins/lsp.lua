40de23d685c644c38f01710fe12fa8603d8ef61d
Faun <git@faun.me>
2024-05-15 14:47:40 -0700

    Remove emoji completion

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -146,7 +146,6 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
-      "hrsh7th/cmp-emoji",
       "hrsh7th/cmp-nvim-lsp",
       "hrsh7th/cmp-nvim-lua",
       "neovim/nvim-lspconfig",
