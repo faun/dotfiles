35c4f518b158e0787a4820177752059869143bf2
Faun <git@faun.me>
2024-05-20 16:46:30 -0700

    Add packages

--- install/03_npm_packages.sh
+++ install/03_npm_packages.sh
@@ -5,33 +5,34 @@ cd "$(dirname "$0")" || exit 1
 # DIR="$(pwd)"
 
 if ! command -v yarn >/dev/null; then
-	npm install -g yarn
+  npm install -g yarn
 fi
 
 npm_packages=(
-	babel-eslint
-	bash-language-server
-	csslint
-	diagnostic-languageserver
-	diff-so-fancy
-	eslint
-	eslint-config-prettier
-	eslint-plugin-prettier
-	eslint-plugin-react
-	htmlhint
-	neovim
-	nginxbeautifier
-	open-cli
-	prettier
-	strip-ansi-cli
-	stylelint
-	tern
-	vim-language-server
-	vscode-langservers-extracted
-	yaml-language-server
+  babel-eslint
+  bash-language-server
+  csslint
+  diagnostic-languageserver
+  diff-so-fancy
+  editorconfig-checker
+  eslint
+  eslint-config-prettier
+  eslint-plugin-prettier
+  eslint-plugin-react
+  htmlhint
+  neovim
+  nginxbeautifier
+  open-cli
+  prettier
+  strip-ansi-cli
+  stylelint
+  tern
+  vim-language-server
+  vscode-langservers-extracted
+  yaml-language-server
 )
 
 for package in "${npm_packages[@]}"; do
-	echo "Installing package: $package"
-	yarn global add "$package" --silent --no-progress --no-emoji 2>/dev/null || true
+  echo "Installing package: $package"
+  yarn global add "$package" --silent --no-progress --no-emoji 2>/dev/null || true
 done
