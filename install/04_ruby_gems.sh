40b858ad17e1d799b9716aaefcbe63b0a454599a
Faun <git@faun.me>
2024-05-31 15:03:52 -0700

    Add additional packages

--- install/04_ruby_gems.sh
+++ install/04_ruby_gems.sh
@@ -5,12 +5,14 @@ cd "$(dirname "$0")" || exit 1
 # DIR="$(pwd)"
 
 rubygems_packages=(
-  erb-formatter
-  neovim
-  reek
-  ripper-tags
+	erb-formatter
+	neovim
+	neovim-ruby-host
+	reek
+	ripper-tags
 )
 for gem in "${rubygems_packages[@]}"; do
-  echo "Installing gem: $gem"
-  rbenv each gem install "$gem" 2>/dev/null || gem install "$gem"
+	echo "Installing gem: $gem"
+	rbenv each gem install "$gem" 2>/dev/null || gem install "$gem"
+	rbenv rehash 2>/dev/null || true
 done
--- install/python_packages.sh
+++ install/python_packages.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 pip_packages=(
-  black
-  cmakelang
-  djlint
-  isort
-  pynvim
-  semgrep
-  yamllint
+	black
+	cmakelang
+	djlint
+	isort
+	pynvim
+	semgrep
+	yamllint
+	proselint
 )
 for egg in "${pip_packages[@]}"; do
-  echo "Installing egg: $egg"
-  pip3 install "$egg"
+	echo "Installing egg: $egg"
+	pip3 install "$egg"
 done
