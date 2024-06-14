9436c768ea2cd9bb6beb3e8cf42c4ec5d15a7d7b
Faun <git@faun.me>
2024-06-14 12:14:36 -0700

    Remove comments

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -11,9 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Example using a list of specs with the default options
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 require("config.lazy")
 require("config.defaults")
