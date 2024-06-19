85d9fa984322dec72a6169d62fdcfcd4323b23ca
Faun <git@faun.me>
2024-06-19 14:30:40 -0700

    Replace neoconf with lazydev

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,27 @@
 return {
-  { "folke/neodev.nvim", opts = {} },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "luvit-meta/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+  { -- optional completion source for require statements and module annotations
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, {
+        name = "lazydev",
+        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+      })
+    end,
+  },
+  { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
   { "folke/neoconf.nvim", cmd = "Neoconf", config = false },
   {
     "williamboman/mason.nvim",
@@ -37,7 +59,6 @@ return {
     event = "LazyFile",
     dependencies = {
       { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
-      { "folke/neodev.nvim", opts = {} },
       "mason.nvim",
       "williamboman/mason-lspconfig.nvim",
     },
@@ -138,8 +159,6 @@ return {
       }
     end,
     config = function()
-      require("neodev").setup({})
-
       local lspconfig = require("lspconfig")
 
       --Enable (broadcasting) snippet capability for completion
