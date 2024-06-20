79ef467f8e08e241b9e7866363ae9661eb551c79
Faun <git@faun.me>
2024-06-20 13:52:40 -0700

    Add yaml.nvim

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -287,6 +287,14 @@ require("lazy").setup({
       ft = { "go", "gomod" },
       build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     },
+    {
+      "cuducos/yaml.nvim",
+      ft = { "yaml" },
+      dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-telescope/telescope.nvim",
+      },
+    },
     { import = "plugins" },
   },
   defaults = {
