d7f25a4ed0fd2e28d396c3e53bfe6856f515a77c
Faun <git@faun.me>
2024-08-02 10:46:45 -0700

    Disable GVM by default

--- shrc/03_gvm.sh
+++ shrc/03_gvm.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 USE_GVM="${USE_GVM:-false}"
-if [[ "$USE_GVM" == "false" ]]; then
-	return
+if [[ "$USE_GVM" != "false" ]]; then
+	# shellcheck source=/dev/null
+	[[ -f "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
+
+	GOROOT="${GOROOT:-${HOME:?}/src}"
+	export GOROOT
+
+	GOPATH="${HOME:?}/go"
+	export GOPATH
+
+	GOBIN="${GOPATH:?}/bin"
+	export GOBIN
+
+	PATH="${PATH}:${GOBIN}"
+	export PATH
 fi
-
-# shellcheck source=/dev/null
-[[ -f "$HOME/.gvm/scripts/gvm" ]] && source "$HOME/.gvm/scripts/gvm"
-
-GOROOT="${GOROOT:-${HOME:?}/src}"
-export GOROOT
-
-GOPATH="${HOME:?}/go"
-export GOPATH
-
-GOBIN="${GOPATH:?}/bin"
-export GOBIN
-
-PATH="${PATH}:${GOBIN}"
-export PATH
