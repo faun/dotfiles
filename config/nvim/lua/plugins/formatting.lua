a5551f6dc06f4235ea11390a6b9dca586ba87094
Faun Winter <faun.winter@gusto.com>
2024-08-23 09:17:43 -0700

    Toggle auto-formatting with <leader>= keymaps

--- config/nvim/lua/plugins/formatting.lua
+++ config/nvim/lua/plugins/formatting.lua
@@ -41,7 +41,73 @@ return {
         mode = "",
         desc = "Format buffer",
       },
+      {
+        -- FormatDisable will disable formatting just for this buffer
+        "<leader>=D",
+        function()
+          -- Call the command with a bang to disable formatting just for this buffer
+          vim.cmd("FormatDisable!")
+        end,
+        mode = "",
+        desc = "Disable autoformat-on-save for this buffer",
+      },
+      {
+        -- FormatDisable will disable formatting globally
+        "<leader>=d",
+        function()
+          vim.cmd("FormatDisable")
+        end,
+        mode = "",
+        desc = "Disable autoformat-on-save globally",
+      },
+
+      {
+        "<leader>=e",
+        function()
+          vim.cmd("FormatEnable")
+        end,
+        mode = "",
+        desc = "Enable autoformat-on-save",
+      },
     },
+    init = function()
+      require("conform").setup({
+        format_on_save = function(bufnr)
+          -- Disable with a global or buffer-local variable
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+          end
+          return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
+      })
+
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+          print("Autoformat-on-save disabled for this buffer")
+        else
+          vim.g.disable_autoformat = true
+          print("Autoformat-on-save disabled globally")
+        end
+      end, {
+        desc = "Disable autoformat-on-save",
+        bang = true,
+      })
+      vim.api.nvim_create_user_command("FormatEnable", function(args)
+        if args.bang then
+          -- FormatEnable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = false
+          print("Autoformat-on-save enabled for this buffer")
+        else
+          vim.g.disable_autoformat = false
+          print("Autoformat-on-save enabled globally")
+        end
+      end, {
+        desc = "Re-enable autoformat-on-save",
+        bang = true,
+      })
+    end,
   },
   {
     "zapling/mason-conform.nvim",
