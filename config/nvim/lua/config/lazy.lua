aa073977378dc0943c9719493f3aea6709ecd525
Faun Winter <faun.winter@gusto.com>
2024-08-23 09:16:19 -0700

    Add nvim-config-local

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -47,6 +47,23 @@ require("lazy").setup({
     { "jghauser/mkdir.nvim" },
     { "jake-stewart/jfind.nvim", branch = "2.0" },
     {
+      "klen/nvim-config-local",
+      config = function()
+        require("config-local").setup({
+          -- Config file patterns to load (lua supported)
+          config_files = { ".nvim.lua", ".nvimrc", ".exrc", ".local.lua" },
+
+          -- Where the plugin keeps files data
+          hashfile = vim.fn.stdpath("data") .. "/config-local",
+
+          autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+          commands_create = true, -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalIgnore)
+          silent = false, -- Disable plugin messages (Config loaded/ignored)
+          lookup_parents = false, -- Lookup config files in parent directories
+        })
+      end,
+    },
+    {
       "mcauley-penney/tidy.nvim",
       opts = {
         enabled_on_save = true,
