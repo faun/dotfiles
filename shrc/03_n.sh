c3731238e45b311a52f535b5869f7a48677b9058
Faun <git@faun.me>
2024-08-23 09:43:00 -0700

    Fall back to nvm if n isn't present

--- shrc/03_n.sh
+++ shrc/03_n.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# Use n by default
 N_PREFIX="${N_PREFIX:-$HOME/n}"
 if [[ -d "$N_PREFIX" ]]; then
-  export N_PREFIX
-  PATH="$N_PREFIX/bin:$PATH"
-  export PATH
+	export N_PREFIX
+	PATH="$N_PREFIX/bin:$PATH"
+	export PATH
+else
+	# Fall back to NVM
+	export NVM_DIR="$([ "${XDG_CONFIG_HOME-}" = "" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 fi
