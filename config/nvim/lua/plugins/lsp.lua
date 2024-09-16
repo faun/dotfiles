981507b5e76e31884ffb9ffc3c4c6336ca2c8265
Faun <git@faun.me>
2024-09-16 09:36:58 -0700

    Use cmp LSP client capabilities

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -160,9 +160,9 @@ return {
     end,
     config = function()
       local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       --Enable (broadcasting) snippet capability for completion
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities.textDocument.completion.completionItem.snippetSupport = true
 
       local on_attach = function(client, bufnr)
