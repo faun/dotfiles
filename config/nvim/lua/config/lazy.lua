a10963b1b74dc42da26d8035a3eef4f1082bbd3a
Faun <git@faun.me>
2024-10-03 09:51:49 -0700

    Set keymaps for mini.surround

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -162,7 +162,20 @@ require("lazy").setup({
     },
     {
       "echasnovski/mini.surround",
-      opts = {},
+      opts = {
+        mappings = {
+          add = "sa", -- Add surrounding in Normal and Visual modes
+          delete = "sd", -- Delete surrounding
+          find = "sf", -- Find surrounding (to the right)
+          find_left = "sF", -- Find surrounding (to the left)
+          highlight = "sh", -- Highlight surrounding
+          replace = "cs", -- Replace surrounding
+          update_n_lines = "sn", -- Update `n_lines`
+
+          suffix_last = "l", -- Suffix to search with "prev" method
+          suffix_next = "n", -- Suffix to search with "next" method
+        },
+      },
       version = "*",
     },
     {
