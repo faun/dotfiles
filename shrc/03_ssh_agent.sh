e7c02184d70afcac2b18f6c8a68216b16ea009a1
Faun <git@faun.me>
2024-10-04 21:56:59 +0000

    Allow SSH_AUTH_SOCK take precedence over ssh-agent

--- shrc/03_ssh_agent.sh
+++ shrc/03_ssh_agent.sh
@@ -4,10 +4,12 @@ function start_agent {
 	/usr/bin/ssh-agent | sed 's/^echo/#echo/' >"${SSH_ENV}"
 	chmod 600 "${SSH_ENV}"
 	source "${SSH_ENV}" >/dev/null
-	ssh-add -A &>/dev/null
+	ssh-add -A &>/dev/null || ssh-add -l
 }
 
-if [[ -d "$HOME/.ssh" ]]; then
+if [[ -n "$SSH_AUTH_SOCK" ]]; then
+	killall ssh-agent &>/dev/null || true
+elif [[ -d "$HOME/.ssh" ]]; then
 	# Source SSH settings, if applicable
 
 	if [ -f "${SSH_ENV}" ]; then
