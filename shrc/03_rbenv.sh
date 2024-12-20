6caaa51c031fc11420b406fe36ecd2970b16fa66
Faun Winter <faun.winter@gusto.com>
2024-12-20 12:10:54 -0800

    Don't assume rbenv

--- shrc/03_rbenv.sh
+++ shrc/03_rbenv.sh
@@ -1,10 +1,10 @@
 # Add rbenv to PATH if it's not already there
 if [[ -z $RBENV_SHELL ]]; then
-	RBENV_ROOT="${RBENV_ROOT:-$HOME/.rbenv}"
+  RBENV_ROOT="${RBENV_ROOT:-$HOME/.rbenv}"
 
-	PATH="$RBENV_ROOT/bin:$PATH"
+  PATH="$RBENV_ROOT/bin:$PATH"
 
-	if command -v rbenv >/dev/null; then
-		eval "$(rbenv init -)"
-	fi
+  if command -v rbenv >/dev/null 2>&1; then
+    eval "$(rbenv init -)"
+  fi
 fi
