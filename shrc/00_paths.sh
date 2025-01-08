d21a88ab7ae0077216335c748846b7d0d779821c
Faun Winter <git@faun.me>
2025-01-08 17:20:26 +0000

    Format file

--- shrc/00_paths.sh
+++ shrc/00_paths.sh
@@ -9,32 +9,32 @@ GOBIN="${GOBIN:-$HOME/bin}"
 export GOBIN
 
 if command -v brew >/dev/null 2>&1; then
-	HOMEBREW_PREFIX="$(brew --prefix)"
+  HOMEBREW_PREFIX="$(brew --prefix)"
 fi
 
 HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-"/opt/homebrew"}"
 export HOMEBREW_PREFIX
 
 if [[ -s "/home/linuxbrew/.linuxbrew/bin/brew" ]]; then
-	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 elif [[ -s "${HOMEBREW_PREFIX:?}/bin/brew" ]]; then
-	eval "$("${HOMEBREW_PREFIX:?}"/bin/brew shellenv)"
+  eval "$("${HOMEBREW_PREFIX:?}"/bin/brew shellenv)"
 fi
 
 if [[ -d "$HOME/.yarn/bin" ]]; then
-	PATH="$PATH:$HOME/.yarn/bin"
+  PATH="$PATH:$HOME/.yarn/bin"
 fi
 
 if [[ -d "$HOME/.config/kubectx" ]]; then
-	PATH="$HOME/.config/kubectx:$PATH"
+  PATH="$HOME/.config/kubectx:$PATH"
 fi
 
 if [[ -d "/usr/local/go/bin" ]]; then
-	PATH="$PATH:/usr/local/go/bin"
+  PATH="$PATH:/usr/local/go/bin"
 fi
 
 if [[ -d "$GOBIN" ]]; then
-	PATH="$PATH:$GOBIN"
+  PATH="$PATH:$GOBIN"
 fi
 
 PATH=".git/safe/../../bin:$PATH"
@@ -42,10 +42,10 @@ PATH=".git/safe/../../node_modules/.bin:$PATH"
 PATH="$HOME/bin:$PATH"
 
 if [[ -d /usr/local/bin ]]; then
-	PATH="/usr/local/bin:$PATH"
+  PATH="/usr/local/bin:$PATH"
 fi
 if [[ -d /usr/local/sbin ]]; then
-	PATH="/usr/local/sbin:$PATH"
+  PATH="/usr/local/sbin:$PATH"
 fi
 
 export PATH
