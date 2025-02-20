339f5054e1878420d1e80e841d6a20458d690925
Faun Winter <faun.winter@gusto.com>
2025-02-20 09:28:24 -0800

    Delegate ctl-r to Atuin if installed

--- zsh/config.sh
+++ zsh/config.sh
@@ -102,7 +102,10 @@ then
 
   bindkey -e                                            # Use emacs key bindings
 
-  bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
+  if ! command -v atuin >/dev/null 2>&1; then
+    bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
+  fi
+
   if [[ "${terminfo[kpp]}" != "" ]]; then
     bindkey "${terminfo[kpp]}" up-line-or-history       # [PageUp] - Up a line of history
   fi
@@ -158,5 +161,4 @@ then
 
   alias help="man zshbuiltins"
   alias reload="reset && source $HOME/.zshrc"
-
 fi
