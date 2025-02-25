05cd0aa989cc5aa950689b7673a641b44f254052
Faun Winter <faun.winter@gusto.com>
2025-02-25 10:44:10 -0800

    Format file

--- install/01_dotfiles.sh
+++ install/01_dotfiles.sh
@@ -7,66 +7,72 @@ cd .. || exit 1
 DIR="$(pwd)"
 
 if ! command -v lndir >/dev/null 2>&1; then
-	echo "lndir is not installed"
-	exit 1
+  echo "lndir is not installed"
+  exit 1
 fi
 
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:=${HOME}/.config}"
 mkdir -p "${XDG_CONFIG_HOME}"
 
 excludes=(
-	LICENSE
-	README.md
-	fonts
-	install.sh
-	install_vim_plugged.sh
-	mac_os_defaults
-	optional
-	setup
-	setup.sh
+  LICENSE
+  README.md
+  fonts
+  install.sh
+  install_vim_plugged.sh
+  mac_os_defaults
+  optional
+  setup
+  setup.sh
 )
 
 for file in ./install/*; do
-	excludes+=("$file")
+  excludes+=("$file")
 done
 
 shouldLinkFile() {
-	for file in "${excludes[@]}"; do
-		if [[ "$file" == "$1" ]]; then
-			return 1
-		fi
-	done
-	return 0
+  for file in "${excludes[@]}"; do
+    if [[ "$file" == "$1" ]]; then
+      return 1
+    fi
+  done
+  return 0
+}
+
+verbose() {
+  echo "$*"
+  $*
 }
 
 linkFile() {
-	name=$1
-	source=$2
-	target=$3
+  name=$1
+  source=$2
+  target=$3
 
-	should_link=$(
-		shouldLinkFile "$name"
-		echo $?
-	)
+  should_link=$(
+    shouldLinkFile "$name"
+    echo $?
+  )
 
-	if [[ $should_link == 0 ]]; then
-		if [[ -L "$target" ]]; then
-			rm "$target"
-		fi
-		echo "Linking $source => $target"
-		if [[ -d "${source:?}" ]]; then
-			mkdir -p "$target"
-			lndir "${source:?}" "$target"
-		fi
-		cp -asf "${source:?}" "$target" || true
-	else
-		echo "Skipping ignored file ${source}"
-	fi
+  if [[ $should_link == 0 ]]; then
+    if [[ -L "$target" ]]; then
+      rm "$target"
+    fi
+    echo "Linking $source => $target"
+    if [[ -d "${source:?}" ]]; then
+      mkdir -p "$target"
+      verbose lndir "${source:?}" "$target"
+    else
+      verbose cp -asf "${source:?}" "$target" || true
+    fi
+  else
+    echo "Skipping ignored file ${source}"
+  fi
 }
 
 for name in *; do
-	source="${DIR:?}/${name:?}"
-	target="$HOME/.$name"
+  source="${DIR:?}/${name:?}"
+  target="$HOME/.$name"
 
-	linkFile $name $source $target
+  linkFile $name $source $target
 done
