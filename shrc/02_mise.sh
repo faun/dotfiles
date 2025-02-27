4ebd2f27add1026180c5b2bba821c6d0a6beabdd
Faun Winter <faun.winter@gusto.com>
2025-02-27 10:38:48 -0800

    Activate mise shims by default

--- shrc/02_mise.sh
+++ shrc/02_mise.sh
@@ -1,4 +1,19 @@
-if command -v mise >/dev/null 2>&1; then
-  SHELL_NAME="$(basename "${SHELL:?}")"
-  eval "$(mise activate ${SHELL_NAME:?})"
+if [[ "${USE_MISE}:-true" != "false" ]]; then
+  MISE_MODE="${MISE_MODE:-shim}"
+
+  if command -v mise >/dev/null 2>&1; then
+    if [[ "${MISE_MODE}" == "shim" ]]; then
+      # shellcheck disable=SC2155
+      export PATH=$(
+        echo "$PATH" |
+          sed -e 's/[^\:]*\/.local\/share\/mise\/shims//' |
+          sed -e 's/::/:/' |
+          sed -e 's/^://' | sed -e 's/:$//'
+      )
+      eval "$(mise activate --shims)"
+    else
+      SHELL_NAME="$(basename "${SHELL:?}")"
+      eval "$(mise activate ${SHELL_NAME:?})"
+    fi
+  fi
 fi
