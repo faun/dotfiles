9282aca54d7bfbaf6d31700a62a1b1d518095904
Faun <git@faun.me>
2025-04-04 15:50:08 -0700

    Add auto-session plugin
    - Add rmagatti/auto-session plugin to manage sessions
    - Disable lazy loading for auto-session
    - Configure suppressed directories for auto-session

--- config/nvim/lua/plugins/lazy.lua
+++ config/nvim/lua/plugins/lazy.lua
@@ -356,4 +356,16 @@ return {
       "nvim-telescope/telescope.nvim",
     },
   },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "/" },
+      -- log_level = 'debug',
+    },
+  },
 }
