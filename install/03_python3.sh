688f89c59db3bad051e92d980e0e6a31ae68fb10
Faun <git@faun.me>
2025-04-04 15:51:28 -0700

    Update indentation in Python3 installation script
    - Adjust indentation to use 2 spaces instead of tabs
    - Ensure consistent formatting across the script

--- install/03_python3.sh
+++ install/03_python3.sh
@@ -17,23 +17,23 @@ set -u
 installed_python_3_version="$(python3 --version 2>/dev/null | sed 's/Python //')"
 
 latest_python_version=$(
-	set +u
-	pyenv install --list |
-		sed 's/^  //' |
-		grep '^3\.' |
-		grep --invert-match 'dev\|a\|b\|rc' |
-		tail -1 2>/dev/null
-	set -u
+  set +u
+  pyenv install --list |
+    sed 's/^  //' |
+    grep '^3\.' |
+    grep --invert-match 'dev\|a\|b\|rc' |
+    tail -1 2>/dev/null
+  set -u
 )
 echo "Latest python version: ${latest_python_version:?}"
 echo "Installed python version: ${installed_python_3_version:?}"
 
 if [[ "${installed_python_3_version:?}" != "${latest_python_version:?}" ]]; then
-	echo "Installing Python ${latest_python_version:?}"
-	set +u
-	pyenv install -s "${latest_python_version:?}"
-	pyenv global "${latest_python_version:?}"
-	set -u
+  echo "Installing Python ${latest_python_version:?}"
+  set +u
+  pyenv install -s "${latest_python_version:?}"
+  pyenv global "${latest_python_version:?}"
+  set -u
 fi
 
 echo "Installing pip and neovim for Python3"
@@ -44,10 +44,10 @@ pyenv virtualenv "${latest_python_version:?}" py3neovim
 set -u
 
 PYTHON3_PATH="$(
-	set +u
-	pyenv activate "${latest_python_version:?}/envs/py3neovim"
-	pyenv which python
-	set -u
+  set +u
+  pyenv activate "${latest_python_version:?}/envs/py3neovim"
+  pyenv which python
+  set -u
 )"
 echo "$PYTHON3_PATH"
 
@@ -57,10 +57,10 @@ set -u
 pip3 install --upgrade pip setuptools wheel neovim >/dev/null
 
 if ! grep "g:python3_host_prog" "$HOME/.vimrc.local" >/dev/null; then
-	echo "Adding Neovim configuration for Python3"
-	echo "let g:python3_host_prog = '$PYTHON3_PATH'" >>"$HOME/.vimrc.local"
+  echo "Adding Neovim configuration for Python3"
+  echo "let g:python3_host_prog = '$PYTHON3_PATH'" >>"$HOME/.vimrc.local"
 else
-	echo "Please remove g:python3_host_prog from $HOME/.vimrc.local and retry"
+  echo "Please remove g:python3_host_prog from $HOME/.vimrc.local and retry"
 fi
 
 echo "Installing debugpy"
