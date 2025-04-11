92031c90f41ae97f4699c9dcbc16e61b3b0e4f8e
Faun <git@faun.me>
2025-04-11 11:24:04 -0700

    Remove unused code

--- config/nvim/lua/plugins/lazy.lua
+++ config/nvim/lua/plugins/lazy.lua
@@ -94,13 +94,6 @@ return {
     end,
   },
   {
-    "mcauley-penney/tidy.nvim",
-    opts = {
-      enabled_on_save = true,
-      filetype_exclude = { "markdown", "diff", "vim" },
-    },
-  },
-  {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
