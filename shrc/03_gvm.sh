343818f077f95e886b5200ea6354cf36f80ad63d
Faun <git@faun.me>
2025-05-28 18:31:19 -0700

    Remove gvm

--- shrc/03_gvm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-USE_GVM="${USE_GVM:-false}"
-if [[ "$USE_GVM" != "false" ]]; then
-  GVM_HOME="${GVM_HOME:-$HOME/.gvm}"
-  if [[ -d "${GVM_HOME}" ]]; then
-    [[ -s "${GVM_HOME:?}/scripts/gvm" ]] && source "${GVM_HOME:?}/scripts/gvm"
-  fi
-fi
