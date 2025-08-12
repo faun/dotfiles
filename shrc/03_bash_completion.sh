ea3bb54ddcfb8539571c5204f55b1291625b3171
Faun <git@faun.me>
2025-08-12 19:27:47 +0000

    Format file

--- shrc/03_bash_completion.sh
+++ shrc/03_bash_completion.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC1090
 
+if [[ "$SHELL" != */bash ]]; then
+  return 0
+fi
+
 HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-}"
 if [[ "$HOMEBREW_PREFIX" == "" ]]; then
-	if type brew &>/dev/null; then
-		HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix)}"
-	fi
+  if type brew &>/dev/null; then
+    HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix)}"
+  fi
 fi
 
 if [ -d "${HOMEBREW_PREFIX}/etc/bash_completion.d" ]; then
-	export BASH_COMPLETION_COMPAT_DIR="${HOMEBREW_PREFIX}/etc/bash_completion.d"
+  export BASH_COMPLETION_COMPAT_DIR="${HOMEBREW_PREFIX}/etc/bash_completion.d"
 fi
 
 if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
-	source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
 fi
