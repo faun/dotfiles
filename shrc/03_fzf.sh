2de82f6d5d57855b1506a2ee59e492c8b9fadd15
Faun <git@faun.me>
2025-10-09 22:36:39 +0000

    Add fzf binary to PATH

--- shrc/03_fzf.sh
+++ shrc/03_fzf.sh
@@ -7,6 +7,8 @@ if [[ $SHELL =~ /zsh$/ ]]; then
 elif [[ $SHELL =~ /bash$/ ]]; then
   [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 fi
+PATH="$HOME/.fzf/bin:$PATH"
+export PATH
 
 export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' --color fg:-1,bg:-1,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B'
 
