0535244cf3852268c6c8eb26e4285c5d804ea59c
Faun <git@faun.me>
2025-10-17 13:02:28 -0700

    Add telescope.nvim plugin with basic configuration

--- config/nvim/lua/plugins/lazy.lua
+++ config/nvim/lua/plugins/lazy.lua
@@ -136,6 +136,22 @@ return {
     },
   },
   {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-h>"] = "which_key",
+            },
+          },
+        },
+      })
+    end,
+  },
+  {
     "nvim-mini/mini.surround",
     opts = {
       mappings = {
