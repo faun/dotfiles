b4a4d38b4ab9d55eb90ae443f830332d7a9a2868
Faun <git@faun.me>
2026-03-24 13:46:12 -0700

    Add OpenTofu format-on-save for LazyVim
    Auto-format .tf and .tfvars files using `tofu fmt` whenever
    a buffer is written, then reload the buffer to pick up changes.

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local tofu_fmt = vim.api.nvim_create_augroup("tofu_fmt", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = tofu_fmt,
+  pattern = { "*.tf", "*.tfvars" },
+  callback = function()
+    vim.cmd("silent !tofu fmt %")
+    vim.cmd("edit")
+  end,
+})
