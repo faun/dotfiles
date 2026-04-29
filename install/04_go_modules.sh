176324d70889ccbfdbe4c683307d3ddc71009a9b
Faun <git@faun.me>
2026-04-29 15:54:02 -0700

    Set GOPROXY default to avoid install failures in restricted networks

--- install/04_go_modules.sh
+++ install/04_go_modules.sh
@@ -6,6 +6,9 @@ cd "$(dirname "$0")" || exit 1
 
 eval "$(mise activate bash)"
 
+GOPROXY="${GOPROXY:-https://proxy.golang.org,direct}"
+export GOPROXY
+
 go_modules=(
   github.com/nametake/golangci-lint-langserver
   golang.org/x/tools/gopls
