3b5b1ab1a4a37511bcd2ebb4409fafa514b283c2
Faun <git@faun.me>
2026-05-13 21:29:58 -0700

    Use -sf flag to allow re-running symlink creation

--- install/05_symlink_1p_socket.sh
+++ install/05_symlink_1p_socket.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 if [[ -d ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t ]]; then
   mkdir -p ~/.1password
-  ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
+  ln -sf ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
 fi
