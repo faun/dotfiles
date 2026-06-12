26a1abe8a04e16bd650713d254f8b8f294c98d39
Faun <git@faun.me>
2026-06-12 09:56:34 -0700

    Source fzf key-bindings so CTRL-T works in zsh
    $SHELL is the login shell path; using [[ $SHELL =~ /zsh$/ ]] with an ERE
    never matches because the pattern requires a literal '/' after the
    end-of-string anchor. Check $ZSH_VERSION / $BASH_VERSION instead, which
    reflects the shell that is actually sourcing this file.

--- shrc/03_fzf.sh
+++ shrc/03_fzf.sh
@@ -2,9 +2,9 @@
 
 # FZF
 # shellcheck disable=1090
-if [[ $SHELL =~ /zsh$/ ]]; then
+if [ -n "$ZSH_VERSION" ]; then
   [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-elif [[ $SHELL =~ /bash$/ ]]; then
+elif [ -n "$BASH_VERSION" ]; then
   [ -f ~/.fzf.bash ] && source ~/.fzf.bash
 fi
 PATH="$HOME/.fzf/bin:$PATH"
